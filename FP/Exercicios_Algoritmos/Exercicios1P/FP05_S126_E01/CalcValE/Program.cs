efb6b283ab2d77c659b916c7f565711f0d9c9d26
William Silva <william.silva@viannasempre.com.br>
2023-09-06 10:32:27 -0300

    Adição de novos exercícios

---
+++ FP/Exercicios_Algoritmos/Exercicios1P/FP05_S126_E01/CalcValE/Program.cs
@@ -0,0 +1,48 @@
+﻿/* Objetivo : Calcular o valor de E.
+ * Autor    : William Silva (https://github.com/unclWill)
+ * Data     : 05/08/2023
+ * Material : FP05
+ * Slide    : 126
+ * Exercício: 01
+ */
+
+/* 1. Escrever um algoritmo que lê um valor N inteiro e positivo e que calcula  * e escreve o valor de E. E = 1 + 1/2 + 1/3 + ... + 1/N
+ */
+
+ using System;
+
+ namespace CalcValE
+ {
+    public class Program
+    {
+        public static void Main()
+        {
+        Console.WriteLine("");
+        ObterValor();
+        Console.Write("\nPressione qualquer tecla para finalizar...");
+        Console.ReadKey();
+        }
+
+        private static void ObterValor()
+        {
+            Console.Write("\n[i] Digite o valor de N: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Calcular(n);
+        }
+
+        private static void Calcular(int n)
+        {
+            int e = 1, acm = 1, e1 = 0;
+
+            while(acm <= n)
+            {
+                e1 = e;
+
+                e = e1 + 1/acm;
+                acm++;
+            }
+
+            Console.WriteLine($"[>] O valor de E é: {e}");
+        }
+    }
+ }
