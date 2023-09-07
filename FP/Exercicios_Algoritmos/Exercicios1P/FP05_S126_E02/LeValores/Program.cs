028aeaf05ef811c428f67f9de91260c966e5b71f
William Silva <william.silva@viannasempre.com.br>
2023-09-07 12:06:51 -0300

    Adição de novos exercícios

---
+++ FP/Exercicios_Algoritmos/Exercicios1P/FP05_S126_E02/LeValores/Program.cs
@@ -0,0 +1,55 @@
+﻿/* Objetivo : Ler 10 valores e armazenar na mesma variável.
+ * Autor    : William Silva (https://github.com/unclWill)
+ * Data     : 05/08/2023
+ * Material : FP05
+ * Slide    : 126
+ * Exercício: 02
+ */
+
+/* 2. Escreva um algoritmo que leia 10 valores (usando a mesma variável) e 
+ * encontre o maior e o menor deles. Mostre o resultado.
+ */
+
+ using System;
+
+ namespace LeValores 
+ {
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Ler valores");
+            LerValor();
+            Console.Write("\nPressione qualquer tecla para finalizar...");
+            Console.ReadKey();
+        }
+
+        private static void LerValor()
+        {
+            int menor = 0, maior = 0;
+
+            for (int i = 1; i < 6; i++) 
+            {
+                Console.Write($"[i] Digite o {i}º valor: ");
+                int valor = Convert.ToInt32(Console.ReadLine());
+
+                if (menor == 0)
+                {
+                    menor = valor;
+                }
+
+                if (valor <= menor && valor < maior) 
+                {
+                    menor = valor;
+                }
+                else if (valor >= maior)
+                {
+                    maior = valor;
+                }
+
+            }
+            Console.WriteLine($"[>] Menor valor: {menor}\n" +
+                              $"[>] Maior valor: {maior}");
+        }
+    }
+ }
