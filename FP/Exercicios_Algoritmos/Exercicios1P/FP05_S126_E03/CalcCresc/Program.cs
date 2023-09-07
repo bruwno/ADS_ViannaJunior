d960abba50d004c0e8fe84dc50de82afd9567035
William Silva <william.silva@viannasempre.com.br>
2023-09-07 12:06:51 -0300

    Adição de novos exercícios

---
+++ FP/Exercicios_Algoritmos/Exercicios1P/FP05_S126_E03/CalcCresc/Program.cs
@@ -0,0 +1,46 @@
+﻿/* Objetivo : Calcular a taxa de crescimento.
+ * Autor    : William Silva (https://github.com/unclWill)
+ * Data     : 05/08/2023
+ * Material : FP05
+ * Slide    : 126
+ * Exercício: 03
+ */
+
+/* 2. Chico tem 1,50 metros e cresce 2 centímetros por ano, enquanto Zé tem 1,40 
+metros e cresce 3 centímetros por ano. Construa um algoritmo que calcule e 
+imprima quantos anos serão necessários para que Zé seja maior que Chico.
+ */
+
+using System;
+
+namespace CalcCresc
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Calcular taxa de crescimento");
+            CalcularTaxaCresc();
+            Console.Write("\nPressione qualquer tecla para finalizar...");
+            Console.ReadKey();
+
+        }
+
+        private static void CalcularTaxaCresc()
+        {
+            double estaturaChico = 1.50;
+            double estaturaZe = 1.40;
+            int tempoDecorrido = 0;
+
+            while (estaturaZe <= estaturaChico)
+            {
+                estaturaChico += 0.02;
+                estaturaZe += 0.03;
+
+                tempoDecorrido++;
+            }
+
+            Console.WriteLine($"\nSerão necessários {tempoDecorrido} anos para que Zé seja maior que Chico.\nChico terá {estaturaChico:N2}m e Zé terá {estaturaZe:N2}m");
+        }
+    }
+}
---
+++ FP/Exercicios_Algoritmos/Exercicios1P/FP05_S126_E04/CalcMedia/Program.cs
@@ -0,0 +1,110 @@
+﻿/* Objetivo : Calcular a taxa de crescimento.
+ * Autor    : William Silva (https://github.com/unclWill)
+ * Data     : 05/08/2023
+ * Material : FP05
+ * Slide    : 126
+ * Exercício: 04
+ */
+
+/* 4. Escreva um algoritmo que leia a matrícula de um aluno e suas três notas. 
+Calcule a média ponderada do aluno, considerando que o peso para a maior 
+nota seja 4 e para as duas restantes, 3. Mostre ao final a média calculada e 
+uma mensagem "APROVADO" se a média for maior ou igual a 5 e 
+"REPROVADO" se a média for menor que 5. Repita a operação até que o 
+código lido seja negativo. 
+*/
+
+using System;
+
+namespace CalcMedia
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Ler dados do aluno");
+            LerDados();
+            Console.Write("\nPressione qualquer tecla para finalizar...");
+            Console.ReadKey();
+        }
+
+        private static void LerDados()
+        {
+            
+        }
+
+        private static double CalculaMedia(double n1, double n2, double n3)
+        {
+            double menorNora = DeterminarMaiorNota(n1, n2, n3);
+            double maiorNota = DeterminarMenorNota(n1, n2, n3);
+            double notaMedia = DeterminarMenorEMaiorNota(n1, n2, n3);
+
+            if () 
+            {
+                
+            }
+
+            double media = maiorNota * media;
+        }
+
+        private static double DeterminarMenorNota(double n1, double n2, double n3)
+        {
+            double menorNota = 0.0;
+
+            if (n1 < n2 && n1 < n3)
+            {
+                maiorNota = n1;
+            }
+            else if (n2 < n1 && n2 < n3)
+            {
+                maiorNota = n2;
+            }
+            else if (n3 < n1 && n3 < n2)
+            {
+                maiorNota = n3;
+            }
+
+            return menorNota;
+        }
+
+        private static double DeterminarMaiorNota(double n1, double n2, double n3)
+        {
+            double maiorNota = 0.0;
+
+            if (n1 > n2 && n1 > n3)
+            {
+                maiorNota = n1;
+            }
+            else if (n2 > n1 && n2 > n3)
+            {
+                maiorNota = n2;
+            }
+            else if (n3 > n1 && n3 > n2)
+            {
+                maiorNota = n3;
+            }
+
+            return maiorNota;
+        }
+
+        private static double DeterminarNotaMedia(double n1, double n2, double n3)
+        {
+            double notaMedia = 0.0;
+
+            if (n1 > n2 && n1 < n3)
+            {
+                notaMedia = n1;
+            }
+            else if (n2 > n1 && n2 > n3)
+            {
+                maiorNota = n2;
+            }
+            else if (n3 > n1 && n3 > n2)
+            {
+                maiorNota = n3;
+            }
+
+            return notaMedia; 
+        }
+    }
+}
