cf03188ecef63716dc467ceed63a3a5585ab5219
William Silva <william.silva@viannasempre.com.br>
2023-09-09 20:39:34 -0300

    Adição de novos exercícios

--- FP/Exercicios_Algoritmos/Exercicios1P/FP05_S126_E04/CalcMedia/Program.cs
+++ FP/Exercicios_Algoritmos/Exercicios1P/FP05_S126_E04/CalcMedia/Program.cs
@@ -9,9 +9,8 @@
 /* 4. Escreva um algoritmo que leia a matrícula de um aluno e suas três notas. 
  * Calcule a média ponderada do aluno, considerando que o peso para a maior 
  * nota seja 4 e para as duas restantes, 3. Mostre ao final a média calculada e 
- * uma mensagem "APROVADO" se a média for maior ou igual a 5 e 
- * "REPROVADO" se a média for menor que 5. Repita a operação até que o 
- * código lido seja negativo. 
+ * uma mensagem "APROVADO" se a média for maior ou igual a 5 e "REPROVADO" se a média for menor que 5.
+ * Repita a operação até que o código lido seja negativo. 
  */
 
 using System;
@@ -30,22 +29,26 @@ namespace CalcMedia
 
         private static void LerDados()
         {
-            int matricula = 0;
-
-            while (matricula > 0)
+            while (true)
             {
                 Console.Write("\n[i] Digite a matrícula do aluno: ");
-                matricula = Convert.ToInt32(Console.ReadLine());
+                int matricula = Convert.ToInt32(Console.ReadLine());
+
+                if (matricula < 0)
+                {
+                    Console.WriteLine("Você escolheu sair...");
+                    break;
+                }
 
                 Console.WriteLine("(i) A maior nota deve ser inserida na 3ª posição.");
                 Console.Write("\n[Peso 3] Digite a 1ª nota: ");
-                double n1 = Convert.ToDouble(Console.ReadLine());
+                double nota1 = Convert.ToDouble(Console.ReadLine());
                 Console.Write("\n[Peso 3] Digite a 2ª nota: ");
-                double n2 = Convert.ToDouble(Console.ReadLine());
+                double nota2 = Convert.ToDouble(Console.ReadLine());
                 Console.Write("\n[Peso 4] Digite a 3ª nota: ");
-                double n3 = Convert.ToDouble(Console.ReadLine());
+                double nota3 = Convert.ToDouble(Console.ReadLine());
 
-                double mediaAluno = CalcularMediaPond(n1, n2, n3);
+                double mediaAluno = CalcularMediaPond(nota1, nota2, nota3);
 
                 if (mediaAluno >= 5.0)
                 {
@@ -56,13 +59,11 @@ namespace CalcMedia
                     Console.WriteLine($"\n[>] Reprovado, com média: {mediaAluno:N2}");
                 }
             }
-
         }
 
         private static double CalcularMediaPond(double n1, double n2, double n3)
         {
-            double media = ((3.0 * n1) + (3.0 * n2) + (4.0 * n3)) / 10.0;
-            return media;
+            return ((3.0 * n1) + (3.0 * n2) + (4.0 * n3)) / 10.0;
         }
     }
 }
