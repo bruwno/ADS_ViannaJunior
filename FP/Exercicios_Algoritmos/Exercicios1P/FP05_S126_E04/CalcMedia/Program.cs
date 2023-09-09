4b8283ad7aa92e98a59cb9861d8f5afca60458e3
William Silva <william.silva@viannasempre.com.br>
2023-09-08 21:53:01 -0300

    Alteração no exercício 04, FP05, S126

--- FP/Exercicios_Algoritmos/Exercicios1P/FP05_S126_E04/CalcMedia/Program.cs
+++ FP/Exercicios_Algoritmos/Exercicios1P/FP05_S126_E04/CalcMedia/Program.cs
@@ -7,12 +7,12 @@
  */
 
 /* 4. Escreva um algoritmo que leia a matrícula de um aluno e suas três notas. 
-Calcule a média ponderada do aluno, considerando que o peso para a maior 
-nota seja 4 e para as duas restantes, 3. Mostre ao final a média calculada e 
-uma mensagem "APROVADO" se a média for maior ou igual a 5 e 
-"REPROVADO" se a média for menor que 5. Repita a operação até que o 
-código lido seja negativo. 
-*/
+ * Calcule a média ponderada do aluno, considerando que o peso para a maior 
+ * nota seja 4 e para as duas restantes, 3. Mostre ao final a média calculada e 
+ * uma mensagem "APROVADO" se a média for maior ou igual a 5 e 
+ * "REPROVADO" se a média for menor que 5. Repita a operação até que o 
+ * código lido seja negativo. 
+ */
 
 using System;
 
