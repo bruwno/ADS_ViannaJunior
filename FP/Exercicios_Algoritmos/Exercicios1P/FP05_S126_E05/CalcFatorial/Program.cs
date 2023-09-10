06eb8541baecfbe0a07dc31bf66a410f3dad5862
William Silva <william.silva@viannasempre.com.br>
2023-09-09 21:38:41 -0300

    Adição de novos exercícios

--- FP/Exercicios_Algoritmos/Exercicios1P/FP05_S126_E05/CalcFatorial/Program.cs
+++ FP/Exercicios_Algoritmos/Exercicios1P/FP05_S126_E05/CalcFatorial/Program.cs
@@ -18,12 +18,34 @@ namespace CalcFatorial
     {
         public static void Main(string[] args)
         {
-
+            Console.WriteLine("Calcular o valor de E");
+            LerValor();
+            Console.Write("\nPressione qualquer tecla para finalizar...");
+            Console.ReadKey();
         }
 
-        private static void CalcularFatorial()
+        private static void LerValor()
         {
+            Console.Write("\n[i] Digite um número inteiro: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
 
+            double resultado = CalcularFatorial(numero);
+            Console.WriteLine($"\nO valor de E é {resultado}");
+        }
+
+        private static double CalcularFatorial(int num)
+        {
+            double e = 1.0, fat = 1.0;
+
+            for (int i = 1; i <= num; i++)
+            {
+                // fatorial * 1,2,3,4..n (tamanho da contagem do laço).
+                fat *= i;
+                // Valor de E = E + 1 / fatorial
+                e += 1.0 / fat;
+            }
+
+            return e;
         }
     }
 }
