ac28260f916cae908a7d37adadf871aed1c82850
William Silva <william.silva@viannasempre.com.br>
2023-09-10 13:26:32 -0300

    Adição de novos exercícios

--- FP/Exercicios_Algoritmos/Exercicios1P/FP05_S127_E08/CalcFatorial/Program.cs
+++ FP/Exercicios_Algoritmos/Exercicios1P/FP05_S127_E08/CalcFatorial/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Objetivo : Calcular fatorial de um número N.
+ * Autor    : William Silva (https://github.com/unclWill)
+ * Data     : 10/09/2023
+ * Material : FP05
+ * Slide    : 127
+ * Exercício: 08
+ */
+
+/* Comando:
+ * 8) Elabore um programa que calcule N! (fatorial de N), sendo que o valor inteiro de
+ * N é fornecido pelo usuário. Sabendo que:
+ * N! = N x (N-1) x (N-2) x ... x 3 x 2 x 1;
+ * 0! = 1, por definição.
+ */
+
+using System;
+
+namespace CalcFatorial
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Caclular fatorial\n");
+            LerValor();
+        }
+
+        private static void LerValor()
+        {
+            Console.Write("[i] Digite um valor inteiro: ");
+            int valor = Convert.ToInt32(Console.ReadLine());
+            double fatorial = CalcularFatorial(valor);
+
+            Console.WriteLine($"O fatorial de {valor}! é {fatorial}");
+        }
+
+        private static double CalcularFatorial(int n)
+        {
+            double fat = 0.0;
+
+            if (n == 0)
+            {
+                return 1.0;
+            }
+
+            for (int i = 1; i < n; i--)
+            {
+                fat *= n - i;
+            }
+
+            return fat;
+        }
+    }
+}
