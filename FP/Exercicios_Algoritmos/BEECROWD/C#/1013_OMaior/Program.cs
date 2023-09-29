0c9a2bf939b54b4308606bdea313ebe0b0f7acd1
William Silva <william.silva@viannasempre.com.br>
2023-09-29 16:30:50 -0300

    Adição de novos exercícios

---
+++ FP/Exercicios_Algoritmos/BEECROWD/C#/1013_OMaior/Program.cs
@@ -0,0 +1,31 @@
+﻿/* Objetivo : Ler três valores e retornar o maior.
+ * Autor    : William Silva (https://github.com/unclWill)
+ * Data     : 29/09/2023
+ * Tipo     : 1 (INICIANTE)
+ * ID       : 1013
+ * Nome     : O Maior
+ * Link     : https://www.beecrowd.com.br/judge/pt/problems/view/1013
+ */
+
+using System;
+class URI
+{
+    static void Main(string[] args)
+    {
+        string[] valoresDigitados = Console.ReadLine().Split(' ');
+        int[] nums = Array.ConvertAll(valoresDigitados, int.Parse);
+
+        int a = nums[0];
+        int b = nums[1];
+        int c = nums[2];
+
+        int maior = (a + b + Math.Abs(a - b)) / 2;
+
+        if (c > maior)
+        {
+            maior = c;
+        }
+
+        Console.WriteLine($"{maior} eh o maior");
+    }
+}
