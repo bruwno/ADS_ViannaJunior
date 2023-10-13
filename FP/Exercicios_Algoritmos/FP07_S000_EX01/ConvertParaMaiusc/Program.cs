76c6f6ad2a999db194df5a1197f79c057799c6b8
William Silva <william.silva@viannasempre.com.br>
2023-10-13 15:33:55 -0300

    Adição de novos exercícios.

---
+++ FP/Exercicios_Algoritmos/FP07_S000_EX01/ConvertParaMaiusc/Program.cs
@@ -0,0 +1,50 @@
+﻿/* Objetivo : Converter os caracteres de uma string para maiúsculas.
+ * Autor    : William Silva (https://github.com/unclWill)
+ * Data     : 13/10/2023
+ * Material : FP07 (Strings)
+ * Slide    : 000 | Aula: https://www.youtube.com/watch?v=tciSBkZvklg
+ * Exercício: 01 (Exemplo)
+ */
+
+/* 1) Faça um procedimento que receba uma string por parâmetro e imprima-a convertida para maiúsculas.
+ */
+
+using System;
+
+namespace ConvertParaMaiusc
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            string str = "exemplo de cadeia de Caracteres";
+
+            ConverterParaMaiuscula(str);
+
+            Console.Write("\n\nPressione qualquer tecla para finalizar...");
+            Console.ReadKey();
+        }
+
+        private static void ConverterParaMaiuscula(string str)
+        {
+            string strConvertida = string.Empty;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                char caractere = str[i];
+
+                if (caractere >= 'a' && caractere <= 'z')
+                {
+                    strConvertida += Convert.ToChar(caractere - 'a' + 'A');
+                }
+                else
+                {
+                    // Preserva os espaços e as letras que já estão maíúsculas (se houverem).
+                    strConvertida += Convert.ToChar(caractere);
+                }
+            }
+
+            Console.WriteLine(strConvertida);
+        }
+    }
+}
