7a14e413b9f88cd6b5a4cd52c2114331d9fcd751
William Silva <william.silva@viannasempre.com.br>
2023-10-14 17:54:52 -0300

    Adição de novos exercícios

--- FP/Exercicios_Algoritmos/FP06_S114_E10/OrdenarVet/Program.cs
+++ FP/Exercicios_Algoritmos/FP06_S114_E10/OrdenarVet/Program.cs
@@ -16,9 +16,48 @@ namespace OrdenarVet
 {
     internal class Program
     {
+        const int n = 5;
+
         internal static void Main(string[] args)
         {
+            int[] vet = new int[n] { 40, 10, 15, 5, 28 };
 
+            /*for (int i = 0, j = 1; i < vet.Length; i++, j++)
+            {
+                Console.Write($"Digite o {j}º elemento de {n}: ");
+                vet[i] = Convert.ToInt32(Console.ReadLine());
+            }*/
+
+            OrdenarValores(vet);
+
+            for (int i = 0; i < vet.Length; i++)
+            {
+                System.Console.Write(vet[i] + " ");
+            }
+
+            Console.Write("\nPressione qualquer tecla para finalizar...");
+            Console.ReadKey();
+        }
+
+        private static void OrdenarValores(int[] vet)
+        {
+            int aux;
+            int[] vetOrd = new int[n];
+
+            for (int i = 0; i < vet.Length; i++)
+            {
+                for (int j = vet.Length - 1; j > i; j--)
+                {
+                    int valorAtual = vet[j], antecessor = vet[j - 1];
+
+                    if (valorAtual < antecessor)
+                    {
+                        aux = vet[j];
+                        vet[j] = vet[j - 1];
+                        vet[j - 1] = aux;
+                    }
+                }
+            }
         }
     }
 }
