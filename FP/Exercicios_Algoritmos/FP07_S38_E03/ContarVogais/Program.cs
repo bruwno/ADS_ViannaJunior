9cd4630be2133245ff6d930f39440135224549b4
William Silva <william.silva@viannasempre.com.br>
2023-10-18 11:41:00 -0300

    Adição de novos exercícios

--- FP/Exercicios_Algoritmos/FP07_S38_E03/ContarVogais/Program.cs
+++ FP/Exercicios_Algoritmos/FP07_S38_E03/ContarVogais/Program.cs
@@ -1,9 +1,9 @@
-﻿/* Objetivo : Contar a quantidade de espaços em uma String e retonar utilizando uma função.
+﻿/* Objetivo : Contar a quantidade de vogais em uma String.
  * Autor    : William Silva (https://github.com/unclWill)
- * Data     : 17/10/2023
+ * Data     : 18/10/2023
  * Material : FP07 (Strings)
  * Slide    : 38
- * Exercício: 02
+ * Exercício: 03
  */
 
 /* 3) Fazer um programa para contar o número de vogais em uma string.
@@ -17,12 +17,28 @@ namespace ContarVogais
     {
         internal static void Main(string[] args)
         {
-            string str = "";
+            char[] vogais = new char[5] { 'a', 'e', 'i', 'o', 'u' };
+            int qtdVogais = 0;
+            string str = "Contando a quantidade de vogais nesta string.";
+            string strConvMinusc = str.ToLower();
 
-            for (int i = 0; i < str.Length; i++) 
+            for (int i = 0; i < strConvMinusc.Length; i++)
             {
-                
+                char caractere = strConvMinusc[i];
+
+                for (int j = 0; j < vogais.Length; j++)
+                {
+                    if (caractere == vogais[j])
+                    {
+                        qtdVogais++;
+                    }
+                }
             }
+
+            Console.WriteLine($"A frase: '{str}' possui {qtdVogais} vogais.");
+
+            Console.Write("\nPressione qualquer tecla para finalizar...");
+            Console.ReadKey();
         }
     }
 }
