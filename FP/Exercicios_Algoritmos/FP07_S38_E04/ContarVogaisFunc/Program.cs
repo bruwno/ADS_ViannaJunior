7077fe3784982ae0ba1ab61e12e034fe964dd6b5
William Silva <william.silva@viannasempre.com.br>
2023-10-18 15:56:02 -0300

    Adição de novos exercícios

---
+++ FP/Exercicios_Algoritmos/FP07_S38_E04/ContarVogaisFunc/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Objetivo : Contar a quantidade de vogais em uma String e retornar em uma função.
+ * Autor    : William Silva (https://github.com/unclWill)
+ * Data     : 18/10/2023
+ * Material : FP07 (Strings)
+ * Slide    : 38
+ * Exercício: 04
+ */
+
+/* 4) Refaça o programa anterior criando uma função que receberá como parâmetro a string 
+ * e retornará o número de vogais que a string contem.
+ */
+
