b55573d875376c4bf0edf8030db6fa67f845ce75
William Silva <william.silva@viannasempre.com.br>
2023-10-18 16:20:16 -0300

    Adição de novos exercícios

--- FP/Exercicios_Algoritmos/FP07_S38_E04/ContarVogaisFunc/Program.cs
+++ FP/Exercicios_Algoritmos/FP07_S38_E04/ContarVogaisFunc/Program.cs
@@ -10,3 +10,61 @@
  * e retornará o número de vogais que a string contem.
  */
 
+using System;
+using System.Text;
+
+namespace ContraVogaisFunc
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            string str = "Contar A quantidade de vogais nesta String.";
+            int qtdVogais = ContarVogais(str);
+
+            Console.WriteLine($"A frase '{str}' possui {qtdVogais} vogais.");
+
+            Console.Write("\nPressione qualquer tecla para finalizar...");
+            Console.ReadKey();
+        }
+
+        private static int ContarVogais(string str)
+        {
+            string strConvertida = ConverterParaMinusculas(str);
+            int qtdVogais = 0;
+
+            for (int i = 0; i < strConvertida.Length; i++)
+            {
+                char caractere = strConvertida[i];
+
+                if (caractere == 'a' || caractere == 'e' || caractere == 'i' || caractere == 'o' || caractere == 'u')
+                {
+                    qtdVogais++;
+                }
+            }
+
+            return qtdVogais;
+        }
+
+        private static string ConverterParaMinusculas(string str)
+        {
+            StringBuilder strConv = new StringBuilder();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                char caractere = str[i];
+
+                if (caractere >= 'A' && caractere <= 'Z')
+                {
+                    strConv.Append(Convert.ToChar(caractere - 'A' + 'a'));
+                }
+                else
+                {
+                    strConv.Append(caractere);
+                }
+            }
+
+            return strConv.ToString();
+        }
+    }
+}
