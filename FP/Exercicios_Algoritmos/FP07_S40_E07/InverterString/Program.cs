981e9c53a834bdb5e1a7929bcd77d4bd25021395
William Silva <william.silva@viannasempre.com.br>
2023-10-19 13:53:30 -0300

    Adição de novos exercícios

---
+++ FP/Exercicios_Algoritmos/FP07_S40_E07/InverterString/Program.cs
@@ -0,0 +1,44 @@
+﻿/* Objetivo : Inverter uma String.
+ * Autor    : William Silva (https://github.com/unclWill)
+ * Data     : 18/10/2023
+ * Material : FP07 (Strings)
+ * Slide    : 40
+ * Exercício: 07
+ */
+
+/* 7) Crie um procedimento que receba uma string e imprima a string invertida.
+ * Exemplo:
+ * Entrada: Teste
+ * Saída: etseT
+ */
+
+ using System;
+ using System.Text;
+
+ namespace InverterString
+ {
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            string str = "Teste";
+            InverteString(str);
+
+            Console.Write("\nPressione qualquer tecla para finalizar...");
+            Console.ReadKey();
+        }
+
+        private static void InverteString(string str)
+        {
+            StringBuilder strInvertida = new StringBuilder();
+
+            for (int i = str.Length - 1; i >= 0; i--) 
+            {
+                char caractere = str[i];
+                strInvertida.Append(caractere); 
+            }
+
+            Console.WriteLine($"String original: {str} - String invertida: {strInvertida.ToString()}");
+        }
+    }
+ }
