212679a55fdf322e6fdf733df96c223634d2088d
William Silva <william.silva@viannasempre.com.br>
2023-10-25 17:10:04 -0300

    Adição de novos exercícios

---
+++ FP/Exercicios_Algoritmos/FP08_S49_E01/MultMatPorEscal/Program.cs
@@ -0,0 +1,49 @@
+﻿/* Objetivo : Multiplicar matriz por escalar.
+ * Autor    : William Silva (https://github.com/unclWill)
+ * Data     : 25/10/2023
+ * Material : FP08 (Matriz)
+ * Slide    : 49
+ * Exercício: 08
+ */
+
+/* 8) Faça um programa que leia duas matrizes quadradas de dimensão 3. 
+ * Crie um procedimento que multiplique as duas matrizes. Imprima o resultado.
+ */
+
+using System;
+
+namespace MultMatPorEscal
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+
+        }
+
+        private static void Multiplicar()
+        {
+
+        }
+
+        private static void ImprimirMatriz()
+        {
+
+        }
+
+        private static double[,] CriarMatriz()
+        {
+            double mat[,] = new double[3, 4];
+
+            for (int i = 0; i < mat.GetLength(0); i++)
+            {
+                for (int j = 0; j < mat.GetLength(1); j++)
+                {
+
+                }
+            }
+
+            return mat;
+        }
+    }
+}
