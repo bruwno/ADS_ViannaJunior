d47fefc9fd85e7aa945bf80ea83c21063c6a5d23
William Silva <william.silva@viannasempre.com.br>
2023-10-25 14:44:59 -0300

    Adição de novos exercícios

--- FP/Exercicios_Algoritmos/FP08_S41_E04/InverteMatriz/Program.cs
+++ FP/Exercicios_Algoritmos/FP08_S41_E04/InverteMatriz/Program.cs
@@ -10,4 +10,54 @@
  * colunas em uma segunda matriz de mesmo tamanho. Imprima ao final a segunda matriz.
  */
 
+using System;
 
+namespace InverteMatriz
+{
+    internal class Program
+    {
+        const int dim = 10;
+        internal static void Main(string[] args)
+        {
+            double[,] mat = new double[dim, dim];
+
+            Console.WriteLine("Informe os valores da matriz:");
+            for (int i = 0; i < mat.GetLength(0); i++)
+            {
+                for (int j = 0; j < mat.GetLength(1); j++)
+                {
+                    Console.Write($"Elemento {i},{j}: ");
+                    mat[i, j] = Convert.ToDouble(Console.ReadLine());
+                }
+            }
+
+            InverterMatriz(mat);
+
+            Console.Write("\nPressione qualquer tecla para finalizar...");
+            Console.ReadKey();
+        }
+
+        private static void InverterMatriz(double[,] mat)
+        {
+            double[,] matInvert = new double[dim, dim];
+
+            for (int i = 0; i < matInvert.GetLength(0); i++)
+            {
+                for (int j = 0; j < matInvert.GetLength(1); j++)
+                {
+                    matInvert[j, i] = mat[i, j];
+                }
+            }
+
+            Console.WriteLine("\nImprimindo os valores (col|lin):");
+            for (int k = 0; k < matInvert.GetLength(0); k++)
+            {
+                for (int l = 0; l < matInvert.GetLength(1); l++)
+                {
+                    Console.Write($"{matInvert[k, l]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
