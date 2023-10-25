440753b155ccfd903375a2063ccf2b405fe8f7f8
William Silva <william.silva@viannasempre.com.br>
2023-10-25 16:43:16 -0300

    Adição de novos exercícios

---
+++ FP/Exercicios_Algoritmos/FP08_S42_E07/MultVetMat/Program.cs
@@ -0,0 +1,59 @@
+﻿/* Objetivo : Multiplicar um vetor por um matriz.
+ * Autor    : William Silva (https://github.com/unclWill)
+ * Data     : 25/10/2023
+ * Material : FP08 (Matriz)
+ * Slide    : 42
+ * Exercício: 06
+ */
+
+/* 7) Faça um programa que leia um vetor de dimensão 5 e uma matriz quadrada de dimensão 5. 
+ * Crie um procedimento que multiplique o vetor pela matriz. Imprima o resultado.
+ */
+
+using System;
+
+namespace MultVetMat
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            double[] vet = new double[5];
+            double[,] mat = new double[5, 5];
+
+            for (int i = 0; i < vet.Length; i++)
+            {
+                Console.Write($"Digite o {i + 1}º valor do vetor: ");
+                vet[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            for (int j = 0; j < mat.GetLength(0); j++)
+            {
+                for (int k = 0; k < mat.GetLength(1); k++)
+                {
+                    Console.Write($"Digite o elemento {j},{k} da matriz: ");
+                    mat[j, k] = Convert.ToDouble(Console.ReadLine());
+                }
+            }
+
+            MultiplicarVetPorMat(vet, mat);
+
+            Console.Write("\nPressione qualquer tecla para finalizar...");
+            Console.ReadKey();
+        }
+
+        private static void MultiplicarVetPorMat(double[] vet, double[,] mat)
+        {
+            System.Console.WriteLine("Resultado: ");
+            for (int i = 0; i < mat.GetLength(0); i++)
+            {
+                for (int j = 0; j < mat.GetLength(1); j++)
+                {
+                    double resultado = vet[i] * mat[i, j];
+                    Console.WriteLine($"{vet[i]} x {mat[i, j]} = {resultado}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
