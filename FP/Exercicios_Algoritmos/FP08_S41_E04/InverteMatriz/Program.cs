d74d37661344f1db5b142fd1aa57f628e6f1a312
William Silva <william.silva@viannasempre.com.br>
2023-10-25 10:27:40 -0300

    Adição de novos exercícios

---
+++ FP/Exercicios_Algoritmos/FP08_S41_E04/InverteMatriz/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Objetivo : Inverter linhas e colunas.
+ * Autor    : William Silva (https://github.com/unclWill)
+ * Data     : 24/10/2023
+ * Material : FP08 (Matriz)
+ * Slide    : 41
+ * Exercício: 04
+ */
+
+/* 4) Faça um programa que leia uma matriz quadrada de tamanho 10 e um procedimento que inverta as linhas pelas 
+ * colunas em uma segunda matriz de mesmo tamanho. Imprima ao final a segunda matriz.
+ */
+
+
