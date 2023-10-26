3b6ba14ec9d8c42efccc12cefc043f23f6125745
William Silva <william.silva@viannasempre.com.br>
2023-10-26 15:12:56 -0300

    Adição de novos exercícios

--- FP/Exercicios_Algoritmos/FP08_S49_E01/MultMatPorEscal/Program.cs
+++ FP/Exercicios_Algoritmos/FP08_S49_E01/MultMatPorEscal/Program.cs
@@ -3,11 +3,13 @@
  * Data     : 25/10/2023
  * Material : FP08 (Matriz)
  * Slide    : 49
- * Exercício: 08
+ * Exercício: 01
  */
 
-/* 8) Faça um programa que leia duas matrizes quadradas de dimensão 3. 
- * Crie um procedimento que multiplique as duas matrizes. Imprima o resultado.
+/* 1) Faça um procedimento para calcular a multiplicação de uma matriz 3 x 4 por um escalar. 
+ * Faça também um procedimento capaz de imprimir esta matriz. 
+ * Ao final, desenvolva uma função principal onde será criada e lida uma matriz 3 x 4. 
+ * Faça nesta função a chamada dos dois procedimentos criados anteriormente.
  */
 
 using System;
@@ -18,32 +20,49 @@ namespace MultMatPorEscal
     {
         internal static void Main(string[] args)
         {
+            double[,] mat = new double[3,4];
 
-        }
-
-        private static void Multiplicar()
-        {
-
-        }
-
-        private static void ImprimirMatriz()
-        {
-
-        }
-
-        private static double[,] CriarMatriz()
-        {
-            double mat[,] = new double[3, 4];
+            Console.Write("Informe o valor escalar: ");
+            double escalar = Convert.ToInt32(Console.ReadLine());
 
             for (int i = 0; i < mat.GetLength(0); i++)
             {
                 for (int j = 0; j < mat.GetLength(1); j++)
                 {
-
+                    Console.Write($"Elemento {i},{j}: ");
+                    mat[i,j] = Convert.ToInt32(Console.ReadLine());
                 }
             }
 
-            return mat;
+            Multiplicar(mat, escalar);
+            ImprimirMatriz(mat);
+
+            Console.Write("\nPressione qualquer tecla para finalizar...");
+            Console.ReadKey();
+        }
+
+        private static void Multiplicar(double[,] mat, double escalar)
+        {
+
+            for (int i = 0; i < mat.GetLength(0); i++)
+            {
+                for (int j = 0; j < mat.GetLength(1); j++)
+                {
+                    mat[i,j] = mat[i,j] * escalar;
+                }
+            }
+        }
+
+        private static void ImprimirMatriz(double[,] mat)
+        {
+            for (int i = 0; i < mat.GetLength(0); i++) 
+            {
+                for (int j = 0; j < mat.GetLength(1); j++) 
+                {
+                    Console.Write($"{mat[i,j]} ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
