d73fc77baa00a42c786bf14e72cce02eaf8f0394
William Silva <william.silva@viannasempre.com.br>
2023-10-30 15:09:55 -0300

    Adição de novos exercícios

---
+++ FP/Exercicios_Algoritmos/FP08_S50_E07/AlunosAprov/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Objetivo : Calcular a média e exibir os alunos aprovados.
+ * Autor    : William Silva (https://github.com/unclWill)
+ * Data     : 30/10/2023
+ * Material : FP08 (Matrizes)
+ * Slide    : 50
+ * Exercício: 07
+ */
+
+/* 7) Crie uma matriz tridimensional onde as linhas indicam as notas de matemática, história e geografia em três provas de
+ * 10 alunos e crie uma função que verifique quantos alunos passaram, ou seja, os que tenham média aritmética > 60 nas3 disciplinas.
+ */
+
+using System;
+
+namespace AlunosAprov
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            double[,] mat = new double[2, 3, 3];
+
+
+
+            Console.Write("\nPressione qualquer tecla para finalizar...");
+            Console.ReadKey();
+        }
+
+        private static double CalcularMedia(double[,] mat)
+        {
+
+        }
+    }
+}
