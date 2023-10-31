bd38129cdd551f964e04f486062c307f3b5d4071
William Silva <william.silva@viannasempre.com.br>
2023-10-31 14:20:21 -0300

    Adição de novos exercícios

--- FBD/Exercicios_SQL/exercicio_create_e_drop.sql
+++ FBD/Exercicios_SQL/exercicio_create_e_drop.sql
@@ -1,33 +1,41 @@
 CREATE DATABASE empresa;
 USE empresa;
 
-CREATE TABLE EMPREGADO(CODIGO_EMPREGADO decimal(6),
-					   NOME varchar(50),
-                       CODIGO_CARGO decimal(5),
-                       ENDERECO varchar(50),
-                       BAIRRO varchar(20),
-                       CIDADE varchar(20),
-                       CODIGO_CHEFE decimal(6),
-                       UF char(2),
-                       DATA_NASC date,
-                       SEXO char(1),
-                       FILIACAO varchar(60),
-                       SALARIO decimal(10));
+CREATE TABLE EMPREGADO (
+    CODIGO_EMPREGADO DECIMAL(6),
+    NOME VARCHAR(50),
+    CODIGO_CARGO DECIMAL(5),
+    ENDERECO VARCHAR(50),
+    BAIRRO VARCHAR(20),
+    CIDADE VARCHAR(20),
+    CODIGO_CHEFE DECIMAL(6),
+    UF CHAR(2),
+    DATA_NASC DATE,
+    SEXO CHAR(1),
+    FILIACAO VARCHAR(60),
+    SALARIO DECIMAL(10 , 2 )
+);
                        
-CREATE TABLE CARGO(CODIGO_CARGO varchar(30),
-				   DESCRICAO_CARGO varchar(30),
-                   SALARIO_MIN numeric(7),
-                   SALARIO_MAX numeric(8),
-                   NIVEL_GRADUACAO char(1));
+CREATE TABLE CARGO (
+    CODIGO_CARGO VARCHAR(30),
+    DESCRICAO_CARGO VARCHAR(30),
+    SALARIO_MIN NUMERIC(7 , 2 ),
+    SALARIO_MAX NUMERIC(8 , 2 ),
+    NIVEL_GRADUACAO CHAR(1)
+);
 
-CREATE TABLE DEPENDENTE(CODIGO_EMPREGADO decimal(6),
-			            NOME_DEPENDENTE varchar(50),
-						SEXO char(1),
-                        DATA_NASC date,
-                        PARENTESCO varchar(15));
+CREATE TABLE DEPENDENTE (
+    CODIGO_EMPREGADO DECIMAL(6),
+    NOME_DEPENDENTE VARCHAR(50),
+    SEXO CHAR(1),
+    DATA_NASC DATE,
+    PARENTESCO VARCHAR(15)
+);
              
-CREATE TABLE DEPARTAMENTO(CODIGO_DEP decimal(3),
-						  NOME_DEP varchar(30),
-                          CODIGO_GERENTE decimal(6),
-                          DATA_INICIO_GERENTE date,
-                          DATA_CRIACAO date);
+CREATE TABLE DEPARTAMENTO (
+    CODIGO_DEP DECIMAL(3),
+    NOME_DEP VARCHAR(30),
+    CODIGO_GERENTE DECIMAL(6),
+    DATA_INICIO_GERENTE DATE,
+    DATA_CRIACAO DATE
+);
