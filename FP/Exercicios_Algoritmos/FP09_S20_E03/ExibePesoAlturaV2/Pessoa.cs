f7e7203c74e75d2f8d2d6184414cf5f2d35e5630
William Silva <william.silva@viannasempre.com.br>
2023-11-07 15:39:52 -0300

    Adição de novos exercícios

---
+++ FP/Exercicios_Algoritmos/FP09_S20_E03/ExibePesoAlturaV2/Pessoa.cs
@@ -0,0 +1,26 @@
+/* Objetivo : Representar o peso e altura de uma pessoa.
+ * Autor    : William Silva (https://github.com/unclWill)
+ * Data     : 01/11/2023
+ * Material : FP09 (Classes)
+ * Slide    : 20
+ * Exercício: 03
+ */
+
+/* 3) Considerando a classe do exercício (2) e a inicialização abaixo:
+ * joao.altura <- 1.90; joao.peso <- 98;
+ * maria.altura <- 1.50; maria.peso <- 55;
+ * - Escreva uma instrução que atribua 1.78 à altura de joao;
+ * - Escreva uma instrução que atribua 75 ao peso de maria.
+ * - Escreva um conjunto de instruções para imprimir a média das alturas e a média dos pesos de joao e maria.
+ */
+
+using System;
+
+namespace ExibePesoAlturaV2
+{
+    public class Pessoa
+    {
+        public double peso;
+        public double altura;
+    }
+}
