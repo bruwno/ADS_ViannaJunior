540ea0627c2a43fed64724606cb4f32766a21492
William Silva <william.silva@viannasempre.com.br>
2023-11-08 16:11:16 -0300

    Adição de novos exercícios

--- FP/Exercicios_Algoritmos/FP09_S43_E08/ListarNomes/Program.cs
+++ FP/Exercicios_Algoritmos/FP09_S43_E08/ListarNomes/Program.cs
@@ -18,8 +18,70 @@ namespace ListarNomes
     {
         internal static void Main(string[] args)
         {
+            Pessoa[] pessoas = new Pessoa[3];
+
+            for (int i = 0; i < pessoas.Length; i++)
+            {
+                pessoas[i] = new Pessoa();
+
+                Console.WriteLine($"\nInforme os dados da {i + 1}ª pessoa");
+
+                pessoas[i].Nome = LerNome();
+                pessoas[i].Idade = LerIdade();
+            }
+
+            OrdenarIdades(pessoas);
+
             Console.Write("\nPressione qualquer tecla para finalizar...");
             Console.ReadKey();
+        }
+
+        private static void OrdenarIdades(Pessoa[] pessoas)
+        {
+            double moverIdade = 0.0;
+            string moverPessoa = string.Empty;
+
+            for (int i = pessoas.Length - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    string pessoaAtual = pessoas[j].Nome;
+                    double idadeAtual = pessoas[j].Idade;
+                    double proxIdade = pessoas[j + 1].Idade;
+
+                    if (idadeAtual > proxIdade)
+                    {
+                        moverIdade = idadeAtual;
+                        moverPessoa = pessoaAtual;
+                        //
+                        pessoas[j].Idade = pessoas[j + 1].Idade;
+                        pessoas[j].Nome = pessoas[j + 1].Nome;
+                        // Move a idade e o nome de posição.
+                        pessoas[j + 1].Idade = moverIdade;
+                        pessoas[j + 1].Nome = moverPessoa;
+                    }
+                }
+            }
+
+            Console.WriteLine("Exibindo os nomes pela ordem de idades");
+            for (int x = 0; x < pessoas.Length; x++)
+            {
+                Console.WriteLine($"Nome: {pessoas[x].Nome} | Idade: {pessoas[x].Idade}");
+            }
+        }
+
+        private static string LerNome()
+        {
+            Console.Write("Digite o nome : ");
+            string nome = Console.ReadLine();
+            return nome;
+        }
+
+        private static double LerIdade()
+        {
+            Console.Write($"Digite a idade: ");
+            double idade = Convert.ToDouble(Console.ReadLine());
+            return idade;
         }
     }
 }
