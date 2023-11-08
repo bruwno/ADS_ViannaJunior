3928a4864cd739a25f5882a7ab71b9f339866b71
William Silva <william.silva@viannasempre.com.br>
2023-11-08 10:20:02 -0300

    Adição de novos exercícios

---
+++ FP/Exercicios_Algoritmos/FP09_S43_E08/ListarNomes/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Objetivo : Listar nomes da menor para a maior idade.
+ * Autor    : William Silva (https://github.com/unclWill)
+ * Data     : 07/11/2023
+ * Material : FP09 (Classes)
+ * Slide    : 43
+ * Exercício: 08
+ */
+
+/* 8) Faça um programa que receba três nomes de no máximo 100 caracteres cada e as idades das respectivas pessoas em um vetor de objetos. 
+ * Após o recebimento, listar os três nomes armazenados neste vetor por ordem crescente de idades.
+ */
+
+ using System;
+
+ namespace ListarNomes
+ {
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            Console.Write("\nPressione qualquer tecla para finalizar...");
+            Console.ReadKey();
+        }
+
+    }
+ }
