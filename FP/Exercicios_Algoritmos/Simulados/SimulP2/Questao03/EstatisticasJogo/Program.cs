184984cb9854a6ee9a4e362809161db6385f0f40
William Silva <william.silva@viannasempre.com.br>
2023-11-15 15:37:14 -0300

    Adição de novos exercícios

--- FP/Exercicios_Algoritmos/Simulados/SimulP2/Questao03/EstatisticasJogo/Program.cs
+++ FP/Exercicios_Algoritmos/Simulados/SimulP2/Questao03/EstatisticasJogo/Program.cs
@@ -18,14 +18,21 @@ namespace EstatisticasJogo
 {
     internal class Program
     {
+        const int qtdJogos = 7;
         internal static void Main(string[] args)
         {
-            for (int i = 0; i < 7; i++)
+            Jogo[] jogos = new Jogo[qtdJogos];
+
+            for (int i = 0; i < jogos.Length; i++)
             {
-                Console.WriteLine($"[JOGO {i + 1} de 7]");
-                Jogo[] jogos = new Jogo[7];
+                Console.WriteLine($"\n[JOGO {i + 1} de {jogos.Length}]");
                 jogos[i] = LerInfoJogo();
             }
+
+            CalcularMediaDeQuedas(jogos);
+
+            Console.Write("\nPressione qualquer tecla para finalizar...");
+            Console.ReadKey();
         }
 
         private static Jogo LerInfoJogo()
@@ -40,6 +47,32 @@ namespace EstatisticasJogo
             jogo.QuedasClerysmar = Convert.ToInt32(Console.ReadLine());
 
             return jogo;
+        }
+
+        private static void CalcularMediaDeQuedas(Jogo[] jogos)
+        {
+            int quedasAbaixoDaMedia = 0;
+            double mediaQuedas = 0.0;
+            double soma = 0.0;
+
+            for (int i = 0; i < jogos.Length; i++)
+            {
+                soma += jogos[i].QuedasClerysmar;
+            }
+
+            mediaQuedas = soma / jogos.Length;
+
+            for (int j = 0; j < jogos.Length; j++)
+            {
+                if (jogos[j].QuedasClerysmar < mediaQuedas)
+                {
+                    Console.WriteLine($"\nNo jogo contra {jogos[j].Adversario}, Clerysmar caiu {jogos[j].QuedasClerysmar} vezes.");
+                    quedasAbaixoDaMedia++;
+                }
+            }
+
+            Console.WriteLine($"\nClerysmar caiu em média {mediaQuedas:N2} vezes por jogo.");
+            Console.WriteLine($"Em {quedasAbaixoDaMedia} jogos Clerysmar caiu menos que o normal.");
         }
     }
 }
