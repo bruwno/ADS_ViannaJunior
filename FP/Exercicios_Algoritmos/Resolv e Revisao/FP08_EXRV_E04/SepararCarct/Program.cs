f791ac670c5c9a17bc83184fe2458bcf188dcef5
William Silva <william.silva@viannasempre.com.br>
2023-11-22 16:39:51 -0300

    Adição de novos exercícios

---
+++ FP/Exercicios_Algoritmos/Resolv e Revisao/FP08_EXRV_E04/SepararCarct/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+
+namespace SepararCarct
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Digite uma string: ");
+            string str = Console.ReadLine();
+
+            Console.WriteLine(SepararCaracteresNumericos(str));
+        }
+
+        public static string SepararCaracteresNumericos(string str)
+        {
+            StringBuilder strConv = new StringBuilder();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (EhDigito(str[i]) == true)
+                {
+                    strConv.Append(str[i]);
+                }
+            }
+
+            return strConv.ToString();
+        }
+
+        public static bool EhDigito(char digito)
+        {
+            const string numeros = "0123456789";
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                if (numeros[i] == digito)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
