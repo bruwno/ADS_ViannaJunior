0135fb7297741cf95d4338d26846235eb8e12e18
William Silva <william.silva@viannasempre.com.br>
2023-11-24 16:55:02 -0300

    Adição de provas do 1ª semestre

---
+++ FP/Exercicios_Algoritmos/Provas/Prova 2/Questao02/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Questao02
+{
+    internal class Program
+    {
+        const int TAM = 10;
+        internal static void Main(string[] args)
+        {
+            Investimento[] investimentos = new Investimento[TAM];
+
+            for (int i = 0; i < investimentos.Length; i++)
+            {
+                Console.Write($"\nInvestimento {i+1} de {TAM}\n");
+                investimentos[i] = LerERetornarDados();
+            }
+
+            Console.Write("\nPesquisar investimentos");
+            Console.Write("\nDigite o tempo em meses: ");
+            int faixaDaPesquisa = Convert.ToInt32(Console.ReadLine());
+            for (int j = 0; j < investimentos.Length; j++)
+            {
+                if (investimentos[j].TempoEmMeses == faixaDaPesquisa)
+                {
+                    Console.WriteLine($"{investimentos[j].Valor:C2}");
+                }
+            }
+        }
+
+        internal static Investimento LerERetornarDados()
+        {
+            Investimento investimento = new Investimento();
+
+            Console.Write("Digite o valor: ");
+            investimento.Valor = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Tempo em meses: ");
+            investimento.TempoEmMeses = Convert.ToInt32(Console.ReadLine());
+
+            return investimento;
+        }
+    }
+}
