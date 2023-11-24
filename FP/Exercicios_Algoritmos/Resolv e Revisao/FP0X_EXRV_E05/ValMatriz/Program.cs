c2639cdb9a6394dfa3e4fce50e5252633b8e8bfa
William Silva <william.silva@viannasempre.com.br>
2023-11-24 17:14:56 -0300

    Adição de novos exercícios

---
+++ FP/Exercicios_Algoritmos/Resolv e Revisao/FP0X_EXRV_E05/ValMatriz/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ValMatriz
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            int[,] mat = new int[3, 3] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write(mat[i, i] + " ");
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(mat[j, j] + " ");
+                }
+                System.Console.WriteLine();
+            }
+        }
+    }
+}
