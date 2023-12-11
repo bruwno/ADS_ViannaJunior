448d966be627c9a38124ecfc1dff2ec844d58629
William Silva <william.silva@viannasempre.com.br>
2023-12-11 12:19:37 -0300

    Adição de novos exercícios

---
+++ 1P/FP/Exercicios_Algoritmos/FP09_S46_E14/GerenciarGado/Program.cs
@@ -0,0 +1,43 @@
+﻿/* Objetivo : Gerenciar dados sobre as cabeças de gado de uma fazenda.
+ * Autor    : William Silva (https://github.com/unclWill)
+ * Data     : 11/12/2023
+ * Material : FP09 (Classes)
+ * Slide    : 46
+ * Exercício: 14
+ */
+
+/* 14) Elabore um programa que auxilie no controle de uma fazenda de gado que possua um total de 100 cabeças de gado.
+ * O programa deverá conter uma classe que comporte:
+ * - código: código da cabeça de gado;
+ * - leite: número de litros de leite produzido por semana;
+ * - alimento: quantidade de alimento ingerida por semana (kg);
+ * - mês de nascimento;
+ * - ano de nascimento;
+ * - abate: ‘N”(não) ou ‘S’ (sim).
+ *
+ * O seu programa deverá conter um menu com as seguintes funcionalidades:
+ * (a) Ler a base de dados (código, leite, alimento, nascimento) informados pelo usuário e armazenar em um vetor de estruturas.
+ * (b) Preencher o campo abate, considerando que a cabeça de gado irá para o abate caso:
+ * - tenha mais de 5 anos, ou;
+ * - produza menos de 40 litros de leite por semana, ou;
+ * - produza entre 50 e 70 litros de leite por semana e ingira mais de 50 quilos de alimento por semana.
+ * (c) Imprimir a quantidade total de leite produzida por semana na fazenda.
+ * (d) Imprimir a quantidade total de alimento consumido por semana na fazenda.
+ * (e) Imprimir a quantidade total de leite que vai ser produzido por semana na fazenda, após o abate
+ * (f) Imprimir a quantidade total de alimento que vai ser consumido por semana na fazenda, após o abate
+ * (g) Imprimir número de cabeças de gado que irão para o abate.
+ * (h) Inclua uma opção para sair do menu.
+ */
+
+using System;
+
+namespace GerenciarGado
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+
+        }
+    }
+}
