1f3775428ed9de53e3bb78eee51f4cc3795b4967
William Silva <william.silva@viannasempre.com.br>
2023-12-11 12:20:09 -0300

    Adição de novos exercícios

--- 1P/FP/Exercicios_Algoritmos/FP09_S46_E14/GerenciarGado/Gado.cs
+++ 1P/FP/Exercicios_Algoritmos/FP09_S46_E14/GerenciarGado/Gado.cs
@@ -14,19 +14,6 @@
  * - mês de nascimento;
  * - ano de nascimento;
  * - abate: ‘N”(não) ou ‘S’ (sim).
- *
- * O seu programa deverá conter um menu com as seguintes funcionalidades:
- * (a) Ler a base de dados (código, leite, alimento, nascimento) informados pelo usuário e armazenar em um vetor de estruturas.
- * (b) Preencher o campo abate, considerando que a cabeça de gado irá para o abate caso:
- * - tenha mais de 5 anos, ou;
- * - produza menos de 40 litros de leite por semana, ou;
- * - produza entre 50 e 70 litros de leite por semana e ingira mais de 50 quilos de alimento por semana.
- * (c) Imprimir a quantidade total de leite produzida por semana na fazenda.
- * (d) Imprimir a quantidade total de alimento consumido por semana na fazenda.
- * (e) Imprimir a quantidade total de leite que vai ser produzido por semana na fazenda, após o abate
- * (f) Imprimir a quantidade total de alimento que vai ser consumido por semana na fazenda, após o abate
- * (g) Imprimir número de cabeças de gado que irão para o abate.
- * (h) Inclua uma opção para sair do menu.
  */
 
 using System;
