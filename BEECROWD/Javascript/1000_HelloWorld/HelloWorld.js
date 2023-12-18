399501d7be7a59374f1681e726bca46a75fcd579
William Silva <william.silva@viannasempre.com.br>
2023-12-18 08:47:52 -0300

    Adição de desafios do Beecrowd

---
+++ BEECROWD/Javascript/1000_HelloWorld/HelloWorld.js
@@ -0,0 +1,16 @@
+/* Objetivo : Exibir a mensagem Hello World! na tela.
+ * Autor    : William Silva (https://github.com/unclWill)
+ * Data     : 18/12/2023
+ * Tipo     : 1 (INICIANTE)
+ * ID       : 1000
+ * Nome     : Hello World
+ * Link     : https://www.beecrowd.com.br/judge/pt/problems/view/1000
+ */
+
+// Para Windows.
+//var lines = require('fs').readFileSync(process.stdin.fd, 'utf8').split('\n');
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+console.log("Hello World!");
