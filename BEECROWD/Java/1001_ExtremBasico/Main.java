b8b52f88c03843152aeb565e7d2342ef25a293e6
William Silva <william.silva@viannasempre.com.br>
2023-12-18 08:09:56 -0300

    Adição de novos exercícios

---
+++ BEECROWD/Java/1001_ExtremBasico/Main.java
@@ -0,0 +1,22 @@
+/* Objetivo : Realizar uma soma e exibir na tela.
+ * Autor    : William Silva (https://github.com/unclWill)
+ * Data     : 18/12/2023
+ * Tipo     : 1 (INICIANTE)
+ * ID       : 1001
+ * Nome     : Hello World
+ * Link     : https://www.beecrowd.com.br/judge/pt/problems/view/1001
+ */
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner readLine = new Scanner(System.in);
+        int a = readLine.nextInt();
+        int b = readLine.nextInt();
+        int result = a + b;
+
+        System.out.println("X = " + result);
+    }
+}
