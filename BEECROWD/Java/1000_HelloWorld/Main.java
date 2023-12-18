0b7a30f5a1f428fe9472e7f67b3c4bfe96fafc5b
William Silva <william.silva@viannasempre.com.br>
2023-12-18 08:09:56 -0300

    Adição de novos exercícios

---
+++ BEECROWD/Java/1000_HelloWorld/Main.java
@@ -0,0 +1,17 @@
+/* Objetivo : Exibir a mensagem Hello World! na tela.
+ * Autor    : William Silva (https://github.com/unclWill)
+ * Data     : 18/12/2023
+ * Tipo     : 1 (INICIANTE)
+ * ID       : 1000
+ * Nome     : Hello World
+ * Link     : https://www.beecrowd.com.br/judge/pt/problems/view/1000
+ */
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World!");
+    }
+}
