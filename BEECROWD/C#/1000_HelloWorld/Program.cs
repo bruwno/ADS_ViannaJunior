42211bcc3edef33c152cf58d76f92c80e0fe0e7f
William Silva <william.silva@viannasempre.com.br>
2023-12-19 12:59:37 -0300

    Adição de novas soluções de problemas do BeeCrowd

---
+++ BEECROWD/C#/1000_HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Objetivo : Exibir a mensagem Hello World! na tela.
+ * Autor    : William Silva (https://github.com/unclWill)
+ * Data     : 18/12/2023
+ * Tipo     : 1 (INICIANTE)
+ * ID       : 1000
+ * Nome     : Hello World
+ * Link     : https://www.beecrowd.com.br/judge/pt/problems/view/1000
+ */
+
+using System;
+
+class URI
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
