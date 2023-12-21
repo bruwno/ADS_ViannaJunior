33bd1c210bcbe5ee33529713dd37ae63a4c5c31b
William Silva <william.silva@viannasempre.com.br>
2023-12-21 08:35:25 -0300

    Adição de novas soluções de problemas do BeeCrowd

---
+++ BEECROWD/C#/3303_Palavrao/Program.cs
@@ -0,0 +1,37 @@
+﻿/* Objetivo : Contar a quantidade de letras em uma palavra e definir se a mesma é "palavrão" ou "palavrinha".
+ * Autor    : William Silva (https://github.com/unclWill)
+ * Data     : 21/12/2023
+ * Tipo     : 1 (INICIANTE)
+ * ID       : 3303
+ * Nome     : Palavrão
+ * Link     : https://www.beecrowd.com.br/judge/pt/problems/view/3303
+ */
+
+using System;
+
+class URI
+{
+    public static void Main(string[] args)
+    {
+        string palavra = Console.ReadLine();
+        DeterminarPalavra(palavra);
+    }
+
+    public static void DeterminarPalavra(string palavra)
+    {
+        int qtdCaracteres = 0;
+        for (int i = 0; i < palavra.Length; i++)
+        {
+            qtdCaracteres++;
+        }
+
+        if (qtdCaracteres >= 10)
+        {
+            Console.WriteLine("palavrao");
+        }
+        else
+        {
+            Console.WriteLine("palavrinha");
+        }
+    }
+}
---
+++ BEECROWD/Java/3303_Palavrao/Main.java
@@ -0,0 +1,33 @@
+/* Objetivo : Contar a quantidade de letras em uma palavra e definir se a mesma é "palavrão" ou "palavrinha".
+ * Autor    : William Silva (https://github.com/unclWill)
+ * Data     : 21/12/2023
+ * Tipo     : 1 (INICIANTE)
+ * ID       : 3303
+ * Nome     : Palavrão
+ * Link     : https://www.beecrowd.com.br/judge/pt/problems/view/3303
+ */
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner readLine = new Scanner(System.in);
+        String palavra = readLine.nextLine();
+        determinarPalavra(palavra);
+        readLine.close();
+    }
+
+    public static void determinarPalavra(String palavra) {
+        int qtdCaracteres = 0;
+        for (int i = 0; i < palavra.length(); i++) {
+            qtdCaracteres++;
+        }
+
+        if (qtdCaracteres >= 10) {
+            System.out.println("palavrao");
+        } else {
+            System.out.println("palavrinha");
+        }
+    }
+}
