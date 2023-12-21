2fe7e4ec404a72b9fd2a368fd7210433698a12a6
William Silva <william.silva@viannasempre.com.br>
2023-12-21 18:45:09 -0300

    Adição de novas soluções de problemas do BeeCrowd

---
+++ BEECROWD/C#/3342_Keanu/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Objetivo : Contar quantas casas brancas e quantas casas pretas um tabuleiro de xadrez com n linhas e colunas possui.
+ * Autor    : William Silva (https://github.com/unclWill)
+ * Data     : 21/12/2023
+ * Tipo     : 1 (INICIANTE)
+ * ID       : 3342
+ * Nome     : Keanu
+ * Link     : https://www.beecrowd.com.br/judge/pt/problems/view/3342
+ */
+
+using System;
+
+class URI
+{
+    public static void Main(string[] args)
+    {
+        int dimensao = int.Parse(Console.ReadLine());
+        int[,] tabuleiro = new int[dimensao, dimensao];
+        int casasBrancas = 0, casasPretas = 0;
+
+        for (int i = 0; i < tabuleiro.GetLength(0); i++)
+        {
+            for (int j = 0; j < tabuleiro.GetLength(1); j++)
+            {
+                if ((i % 2 == 0) && (j % 2 == 0))
+                {
+                    casasBrancas++;
+                }
+                else if ((i % 2 != 0) && (j % 2 != 0))
+                {
+                    casasBrancas++;
+                }
+                else
+                {
+                    casasPretas++;
+                }
+            }
+        }
+        Console.WriteLine($"{casasBrancas} casas brancas e {casasPretas} casas pretas");
+    }
+}
