6963e07591361ee5d93f354076d66f0d08fd5dfc
William Silva <william.silva@viannasempre.com.br>
2023-12-21 21:47:34 -0300

    Adição de novas soluções de problemas do BeeCrowd

---
+++ BEECROWD/Outras/Lua/1003/SomaSimples.lua
@@ -0,0 +1,14 @@
+--[[
+Objetivo: Exibir o resultado da soma de dois inteiros.
+Autor   : William Silva (https://github.com/unclWill)
+Data    : 20/12/2023
+Tipo    : 1 (INICIANTE)
+ID      : 1003
+Nome    : Hello World
+Link    : https://www.beecrowd.com.br/judge/pt/problems/view/1003
+--]]
+
+num1 = io.read("*number")
+num2 = io.read("*number")
+
+io.write("SOMA = ", num1 + num2, "\n")
