ac39a0a5c0c86b685e1d10d6f8974c2c3dab86c9
William Silva <william.silva@viannasempre.com.br>
2023-12-22 17:06:41 -0300

    Adição de novas soluções de problemas do BeeCrowd

---
+++ BEECROWD/C#/1073/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Objetivo: Imprimir o quadrado dos pares até o número n informado.
+ * Autor   : William Silva (https://github.com/unclWill)
+ * Data    : 22/12/2023
+ * Tipo    : 1 (INICIANTE)
+ * ID      : 1073
+ * Nome    : Quadrado de Pares
+ * Link    : https://www.beecrowd.com.br/judge/pt/problems/view/1073
+ */
+
+using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 2; i <= n; i += 2)
+        {
+            int result = (int)Math.Pow(i, 2);
+            Console.WriteLine($"{i}^2 = {result}");
+        }
+    }
+}
