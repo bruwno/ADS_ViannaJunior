762093315def6cd98d00946ba2e42f7630f1dbd3
William Silva <william.silva@viannasempre.com.br>
2023-12-21 22:35:03 -0300

    Adição de novas soluções de problemas do BeeCrowd

---
+++ BEECROWD/Outras/Dart/1000/HelloWorld.dart
@@ -0,0 +1,15 @@
+/* Objetivo: Exibir a mensagem Hello World! na tela.
+ * Autor   : William Silva (https://github.com/unclWill)
+ * Data    : 21/12/2023
+ * Tipo    : 1 (INICIANTE)
+ * ID      : 1000
+ * Nome    : Hello World
+ * Link    : https://www.beecrowd.com.br/judge/pt/problems/view/1000
+ */
+
+import 'dart:io';
+
+void main()
+{
+    print('Hello World!\n');
+}
