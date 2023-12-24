25e731ade4d304c6a51db17894782ffc5d4ecd6b
William Silva <william.silva@viannasempre.com.br>
2023-12-23 22:27:13 -0300

    Adição de novas soluções de problemas do BeeCrowd

---
+++ BEECROWD/Outras/SQL/2608/MaiorEMenorPreco.sql
@@ -0,0 +1,24 @@
+/* Objetivo: Exibir o maior e o menor preço dos produtos da trabela.
+ * Autor   : William Silva (https://github.com/unclWill)
+ * Data    : 23/12/2023
+ * Tipo    : 9 (SQL)
+ * ID      : 2608
+ * Nome    : Maior e Menor Preço
+ * Link    : https://www.beecrowd.com.br/judge/pt/problems/view/2608
+ *
+ * Plataforma utilizada para montar a solução: https://coderpad.io/languages/postgresql/
+ */
+
+ -- Script da tabela
+CREATE TABLE products (id NUMERIC PRIMARY KEY, name VARCHAR, amount NUMERIC, price NUMERIC);
+    INSERT INTO products (id, name, amount, price) VALUES (1, 'Two-doors wardrobe', 100, 800);
+    INSERT INTO products (id, name, amount, price) VALUES (2, 'Dining table', 1000, 560);
+    INSERT INTO products (id, name, amount, price) VALUES (3, 'Towel holder', 10000, 25.50);
+    INSERT INTO products (id, name, amount, price) VALUES (4, 'Computer desk', 350, 320.50);
+    INSERT INTO products (id, name, amount, price) VALUES (5, 'Chair', 3000, 210.64);
+    INSERT INTO products (id, name, amount, price) VALUES (6, 'Single bed', 750, 460);
+
+-- Solução
+SELECT MAX(price) AS price, 
+       MIN(price) AS price
+    FROM products;
