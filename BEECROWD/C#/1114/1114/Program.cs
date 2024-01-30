a61de93dd925a682613e1b0e80bbe1873865f305
William Silva <william.silva@viannasempre.com.br>
2024-01-30 16:23:04 -0300

    Adição de novas soluções

---
+++ BEECROWD/C#/1114/1114/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Objetivo: Testar um valor de entrada.
+ * Autor   : William Silva (https://github.com/unclWill)
+ * Data    : 30/01/2024
+ * Tipo    : 1 (INICIANTE)
+ * ID      : 1114
+ * Nome    : Senha Fixa
+ * Link    : https://www.beecrowd.com.br/judge/pt/problems/view/1114
+ */
+
+using System;
+
+class URI
+{
+    public static void Main(string[] args)
+    {
+        int senha;
+
+        do
+        {
+            senha = int.Parse(Console.ReadLine());
+
+            if (senha != 2002)
+            {
+                Console.WriteLine("Senha Invalida");
+            }
+            else
+            {
+                Console.WriteLine("Acesso Permitido");
+                break;
+            }
+        }
+        while (true);
+    }
+}
---
+++ BEECROWD/Java/1114/Main.java
@@ -0,0 +1,29 @@
+/* Objetivo: Testar um valor de entrada.
+ * Autor   : William Silva (https://github.com/unclWill)
+ * Data    : 30/01/2024
+ * Tipo    : 1 (INICIANTE)
+ * ID      : 1114
+ * Nome    : Senha Fixa
+ * Link    : https://www.beecrowd.com.br/judge/pt/problems/view/1114
+ */
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner readLine = new Scanner(System.in);
+        int senha;
+
+        do {
+            senha = readLine.nextInt();
+            if (senha != 2002) {
+                System.out.println("Senha Invalida");
+            } else {
+                System.out.println("Acesso Permitido");
+                break;
+            }
+        } while (true);
+        readLine.close();
+    }
+}
