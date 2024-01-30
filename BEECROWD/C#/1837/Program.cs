71f4107d309852f0a765adc960415fd71a0a80bb
William Silva <william.silva@viannasempre.com.br>
2024-01-30 19:56:22 -0300

    Adição de novas soluções

---
+++ BEECROWD/C#/1837/Program.cs
@@ -0,0 +1,59 @@
+﻿/* Objetivo: .
+ * Autor   : William Silva (https://github.com/unclWill)
+ * Data    : 30/01/2024
+ * Tipo    : 7 (INICIANTE)
+ * ID      : 1837
+ * Nome    : Prefácio
+ * Link    : https://www.beecrowd.com.br/judge/pt/problems/view/1837
+ */
+
+using System;
+
+class URI
+{
+    public static void Main(string[] args)
+    {
+        string[] entrada = Console.ReadLine().Trim().Split(' ');
+        int a = Convert.ToInt32(entrada[0]);
+        int b = Convert.ToInt32(entrada[1]);
+
+        int q = 0, r = 0;
+
+        if (a >= 0)
+        {
+            q = a / b;
+            r = a % b;
+        }
+        else
+        {
+            int c = 0, d = 0;
+
+            c = TransformarEmPositivo(b);
+
+            for (r = 0; r < c; r++)
+            {
+                d = a - r;
+
+                if (d % b == 0)
+                {
+                    break;
+                }
+            }
+
+            q = d / b;
+        }
+        Console.WriteLine($"{q} {r}");
+    }
+
+    private static int TransformarEmPositivo(int num)
+    {
+        if (num < 0)
+        {
+            return num * -1;
+        }
+        else
+        {
+            return num;
+        }
+    }
+}
