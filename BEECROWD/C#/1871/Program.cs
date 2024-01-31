d8c6cc382522828b83968f43fa4c371d40ef89ab
William Silva <william.silva@viannasempre.com.br>
2024-01-31 19:33:27 -0300

    Adição de novas soluções

---
+++ BEECROWD/C#/1871/Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * Objetivo: Somar valores e, se existirem, remover os zeros dos resultados.
+ * Autor   : William Silva (https://github.com/unclWill)
+ * Data    : 31/01/2024
+ * Tipo    : 3 (STRINGS)
+ * ID      : 1871
+ * Nome    : Zero vale Zero
+ * Link    : https://www.beecrowd.com.br/judge/pt/problems/view/1871
+ */
+
+using System;
+using System.Collections.Generic;
+
+class URI
+{
+    public static void Main(string[] args)
+    {
+        List<string> armazenaSomas = new List<string>();
+
+        while (true)
+        {
+            int soma = 0, i = 0;
+
+            string[] entrada = Console.ReadLine().Trim().Split(' ');
+
+            if (entrada[i].Equals("0") && entrada[i + 1].Equals("0"))
+            {
+                break;
+            }
+            else
+            {
+                soma = int.Parse(entrada[i]) + int.Parse(entrada[i + 1]);
+                armazenaSomas.Add(soma.ToString());
+            }
+            i++;
+        }
+        RemoverZeros(armazenaSomas);
+    }
+
+    private static void RemoverZeros(List<string> valores)
+    {
+        string novoValor = string.Empty;
+
+        foreach (string valor in valores)
+        {
+            if (valor.Contains("0"))
+            {
+                novoValor = valor.Replace("0", "");
+                Console.WriteLine(novoValor);
+            }
+            else
+            {
+                novoValor = valor;
+                Console.WriteLine(novoValor);
+            }
+        }
+    }
+}
