dbf0cda898649718ae379329a81323c3e9245915
William Silva <william.silva@viannasempre.com.br>
2024-01-31 19:48:31 -0300

    Adição de novas soluções

---
+++ BEECROWD/C#/1016/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Objetivo: Calcular a distância percorrida por dois veículos se movendo a velocidades diferentes.
+ * Autor   : William Silva (https://github.com/unclWill)
+ * Data    : 31/01/2024
+ * Tipo    : 1 (INICIANTE)
+ * ID      : 1016
+ * Nome    : Distância
+ * Link    : https://www.beecrowd.com.br/judge/pt/problems/view/1016
+ */
+
+using System;
+
+class URI
+{
+    public static void Main(string[] args)
+    {
+        int distancia = int.Parse(Console.ReadLine());
+        int tempo = distancia * 2;
+        Console.WriteLine($"{tempo} minutos");
+    }
+}
