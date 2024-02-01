015c8d2672044a434f71ba1ee1d5b68ec52e1197
William Silva <william.silva@viannasempre.com.br>
2024-02-01 20:12:19 -0300

    Adição de exemplos de algoritmos recursivos

---
+++ 2P/ESTDAD/Recursividade/MultiplicacaoRecursiva/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MultiplicacaoRecursiva
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Multiplicacao mult = new Multiplicacao();
+            Console.Write($"Resultado = {mult.Multiplicar(7, 5)}");
+        }
+    }
+}
