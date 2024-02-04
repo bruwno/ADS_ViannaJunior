0bfd87295fb70061e2934a669b5603107f545784
William Silva <william.silva@viannasempre.com.br>
2024-02-03 21:53:18 -0300

    Adição de novas soluções de problemas do Beecrowd

---
+++ BEECROWD/C#/1632/Program.cs
@@ -0,0 +1,64 @@
+﻿/* Objetivo: Determinar de quantas maneiras uma senha pode ser formada.
+ * Autor   : William Silva (https://github.com/unclWill)
+ * Data    : 03/02/2024
+ * Tipo    : 3 (STRINGS)
+ * ID      : 1632
+ * Nome    : Variações
+ * Link    : https://www.beecrowd.com.br/judge/pt/problems/view/1632
+ */
+
+using System;
+using System.Collections.Generic;
+
+class URI
+{
+    public static void Main(string[] args)
+    {
+        List<int> resultados = new List<int>();
+        int t = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < t; i++)
+        {
+            string s = Console.ReadLine();
+            resultados.Add(RetornarQtdPossibilidades(s));
+        }
+
+        foreach (int resultado in resultados)
+        {
+            Console.WriteLine(resultado);
+        }
+    }
+
+    private static int RetornarQtdPossibilidades(string str)
+    {
+        int qtdPissib = 1;
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            char caractere = str[i];
+            if (VerificarCaractere(caractere))
+            {
+                qtdPissib *= 3;
+            }
+            else
+            {
+                qtdPissib *= 2;
+            }
+        }
+        return qtdPissib;
+    }
+
+    private static bool VerificarCaractere(char caractere)
+    {
+        const string conjuntoDeCaracteres = "AEIOSaeios";
+
+        for (int i = 0; i < conjuntoDeCaracteres.Length; i++)
+        {
+            if (caractere == conjuntoDeCaracteres[i])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ BEECROWD/Outras/C/1632/Main.c
@@ -0,0 +1,66 @@
+/* Objetivo: Determinar de quantas maneiras uma senha pode ser formada.
+ * Autor   : William Silva (https://github.com/unclWill)
+ * Data    : 03/02/2024
+ * Tipo    : 3 (STRINGS)
+ * ID      : 1632
+ * Nome    : Variações
+ * Link    : https://www.beecrowd.com.br/judge/pt/problems/view/1632
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+bool verificaCaractere(char caractere)
+{
+    const char conjuntoDeCaracteres[] = "AEIOSaeios";
+
+    for (int i = 0; conjuntoDeCaracteres[i] != '\0'; i++)
+    {
+        if (caractere == conjuntoDeCaracteres[i])
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int retornarQtdPossibilidades(char str[])
+{
+    int qtdPossib = 1;
+
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        char caractere = str[i];
+        if (verificaCaractere(caractere))
+        {
+            qtdPossib *= 3;
+        }
+        else
+        {
+            qtdPossib *= 2;
+        }
+    }
+    return qtdPossib;
+}
+
+int main()
+{
+    int resultados[100];
+    int t = 0;
+    scanf("%d", &t);
+
+    for (int i = 0; i < t; i++)
+    {
+        char s[16];
+        scanf("%s", s);
+        resultados[i] = retornarQtdPossibilidades(s);
+    }
+
+    for (int i = 0; i < t; ++i)
+    {
+        printf("%d\n", resultados[i]);
+    }
+
+    return 0;
+}
