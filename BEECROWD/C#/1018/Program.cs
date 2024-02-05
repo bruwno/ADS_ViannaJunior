abb72477d6c47e9ed643245336cf7d1839501881
William Silva <william.silva@viannasempre.com.br>
2024-02-05 16:09:53 -0300

    Adição de novas soluções de problemas do Beecrowd

---
+++ BEECROWD/C#/1018/Program.cs
@@ -0,0 +1,37 @@
+﻿/* Objetivo: Determinar a quantidade de notas necessárias para formar um valor.
+ * Autor   : William Silva (https://github.com/unclWill)
+ * Data    : 05/02/2024
+ * Tipo    : 1 (INICIANTE)
+ * ID      : 1018
+ * Nome    : Cédulas
+ * Link    : https://www.beecrowd.com.br/judge/pt/problems/view/1018
+ */
+
+using System;
+
+class URI
+{
+    const int Nota100 = 100, Nota50 = 50, Nota20 = 20, Nota10 = 10, Nota5 = 5, Nota2 = 2, Nota1 = 1;
+
+    public static void Main(string[] args)
+    {
+        int valor = int.Parse(Console.ReadLine());
+        int valorOriginal = valor;
+
+        int[] notas = { Nota100, Nota50, Nota20, Nota10, Nota5, Nota2, Nota1 };
+        int[] qtdNotas = new int[notas.Length];
+
+        for (int i = 0; i < notas.Length; i++)
+        {
+            qtdNotas[i] = valor / notas[i];
+            valor %= notas[i];
+        }
+
+        Console.WriteLine($"{valorOriginal}");
+
+        for (int i = 0; i < notas.Length; i++)
+        {
+            Console.WriteLine($"{qtdNotas[i]} nota(s) de R$ {notas[i]},00");
+        }
+    }
+}
---
+++ BEECROWD/Java/1018/Main.java
@@ -0,0 +1,35 @@
+/* Objetivo: Determinar a quantidade de notas necessárias para formar um valor.
+ * Autor   : William Silva (https://github.com/unclWill)
+ * Data    : 05/02/2024
+ * Tipo    : 1 (INICIANTE)
+ * ID      : 1018
+ * Nome    : Cédulas
+ * Link    : https://www.beecrowd.com.br/judge/pt/problems/view/1018
+ */
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    static final int NOTA100 = 100, NOTA50 = 50, NOTA20 = 20, NOTA10 = 10, NOTA5 = 5, NOTA2 = 2, NOTA1 = 1;
+
+    public static void main(String[] args) throws IOException {
+        Scanner readLine = new Scanner(System.in);
+        int valor = Integer.parseInt(readLine.nextLine());
+        int valorOriginal = valor;
+        readLine.close();
+        int[] notas = new int[] { NOTA100, NOTA50, NOTA20, NOTA10, NOTA5, NOTA2, NOTA1 };
+        int[] qtdNotas = new int[notas.length];
+
+        for (int i = 0; i < notas.length; i++) {
+            qtdNotas[i] = valor / notas[i];
+            valor %= notas[i];
+        }
+
+        System.out.printf("%d\n", valorOriginal);
+
+        for (int i = 0; i < notas.length; i++) {
+            System.out.printf("%d nota(s) de R$ %d,00\n", qtdNotas[i], notas[i]);
+        }
+    }
+}
