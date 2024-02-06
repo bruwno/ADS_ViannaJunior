ed6c4322b762741182a78c45bb149f07c58ccec1
William Silva <william.silva@viannasempre.com.br>
2024-02-06 17:24:30 -0300

    Adição de atividades da disciplina de TWA

--- 2P/TWA/_RecursosExternos/ExplorandoJavaScript_Alura/4-conversaoDeTipos.js
+++ 2P/TWA/_RecursosExternos/ExplorandoJavaScript_Alura/4-conversaoDeTipos.js
@@ -2,10 +2,17 @@ console.log("Conversão de Tipos");
 
 console.log("ano " + 2020);
 console.log("20" + "10");
+
 // Conversão explícita.
 console.log(parseInt("20") + parseInt("10"));
 
-// Conversão implícita. O interpretador automaticamente tenta converter as Strings
+console.log(parseFloat("20.5") * parseFloat("10"));
+
+// Conversão implícita. 
+// O interpretador automaticamente tenta converter as Strings
 // para number a fim de realizar a operação, o que não ocorre com operações de SOMA, 
 // uma vez que o operador + possui também a função de concatentar textos.
 console.log("10" / "2");
+
+// NaN - O interpretador não entende a operação.
+console.log("Pizza" / "2");
