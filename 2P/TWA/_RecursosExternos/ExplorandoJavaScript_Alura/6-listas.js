80d235f1d61258f4bbf89a20f7a075025bf5c916
William Silva <william.silva@viannasempre.com.br>
2024-02-06 20:53:44 -0300

    Adição de atividades da disciplina de TWA

--- 2P/TWA/_RecursosExternos/ExplorandoJavaScript_Alura/6-listas.js
+++ 2P/TWA/_RecursosExternos/ExplorandoJavaScript_Alura/6-listas.js
@@ -1,3 +1,26 @@
-// Trabalhando com Listas.
+// Documentação: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array
 
-console.log();
+console.log(`Trabalhando com listas`);
+
+// Declaração de Arrays.
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`,
+);
+
+// Adicionando um item na lista dinamicamente, depois dela já ter sido decalarada.
+listaDeDestinos.push(`Juiz de Fora`);
+listaDeDestinos.push(`Belo Horizonte`);
+
+console.log("Destinos possíveis: ");
+console.log(listaDeDestinos);
+
+// Removendo um item da lista com o método slice(). 
+/* Os argumentos passados são o índice do item e quantos itens devem ser deletados.
+ */
+listaDeDestinos.splice(1, 1);
+console.log(listaDeDestinos);
+
+// Exibindo um índice específico da lista.
+console.log(listaDeDestinos[1]);
