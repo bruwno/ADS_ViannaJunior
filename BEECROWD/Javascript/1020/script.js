d94c75e26cda8f8b0e1474093e68c762e477b2b3
William Silva <william.silva@viannasempre.com.br>
2024-02-07 15:49:08 -0300

    Adição de novas soluções de problemas do Beecrowd

---
+++ BEECROWD/Javascript/1020/script.js
@@ -0,0 +1,9 @@
+/* Objetivo: Converter uma entrada de idade em dias para anos, meses e dias.
+ * Autor   : William Silva (https://github.com/unclWill)
+ * Data    : 07/02/2024
+ * Tipo    : 1 (INICIANTE)
+ * ID      : 1020
+ * Nome    : Idade em Dias
+ * Link    : https://www.beecrowd.com.br/judge/pt/problems/view/1020
+ */
+
