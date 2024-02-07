dafe9bd71db93e4b376a2b34ea4b486c5b702d18
William Silva <william.silva@viannasempre.com.br>
2024-02-07 16:17:25 -0300

    Adição de novas soluções de problemas do Beecrowd

--- BEECROWD/Javascript/1020/script.js
+++ BEECROWD/Javascript/1020/script.js
@@ -7,3 +7,23 @@
  * Link    : https://www.beecrowd.com.br/judge/pt/problems/view/1020
  */
 
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('', (idadeEmDias) => {
+    converterIdade(parseInt(idadeEmDias));
+    rl.close();
+});
+
+function converterIdade(idadeEmDias) {
+    const anos = Math.floor(idadeEmDias / 365);
+    const diasRestantes = idadeEmDias % 365;
+    const meses = Math.floor(diasRestantes / 30);
+    const dias = diasRestantes % 30;
+    console.log(`${anos} ano(s)\n${meses} mes(es)\n${dias} dia(s)`);
+}
+
