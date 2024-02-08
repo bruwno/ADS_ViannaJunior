8fa6ed280f7d5d639e2685f22160f4f68b951214
William Silva <william.silva@viannasempre.com.br>
2024-02-08 10:57:38 -0300

    Adição de atividades da disciplina de EST (Estrutura de Dados)

---
+++ 2P/EST/Aulas/Aula01/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Aula01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var nome = "William";
+            var idade = 26;
+            string texto = string.Format("Nome: {0}, idade: {1}", nome, idade);
+            Console.WriteLine(texto);
+
+            var txt = "@bcde";
+            var txtCorrigido = texto.Replace('@', 'a');
+            Console.WriteLine(txtCorrigido);
+
+            // Passando um array para o método Formatar.
+            string[] valores = new string[] { "William", "26" };
+            Console.WriteLine(Formatar(texto, valores));
+        }
+
+        public static string Formatar(string texto, params string[] valores)
+        {
+            for (int i = 0; i < valores.Length; i++)
+            {
+                texto = texto.Replace("{" + i + "}", valores[i]);
+            }
+            return texto;
+        }
+    }
+}
---
+++ 2P/EST/Aulas/Aula01_Ex02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula01_Ex02 
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
