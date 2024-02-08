89c4c2feab0815ca902d15c628d9e62edf99f7f5
William Silva <william.silva@viannasempre.com.br>
2024-02-08 08:46:54 -0300

    Adição de atividades da disciplina de TWA

--- 2P/TWA/Exercicios/Topico1/Aula05_02/script.js
+++
@@ -1,8 +0,0 @@
-/*
- * DATA   : 05/02/2024
- * AUTOR  : William Silva (https://github.com/unclWill)
- * ASSUNTO: Tópico 1 - Introdução ao JavaScript (puro)
- */
-
-nome = "William";
-console.log("Olá " + nome + "!");
