e8c752f3daa8a4f38d3fa8d8cbb76bba1687fb1a
William Silva <william.silva@viannasempre.com.br>
2024-02-13 17:23:18 -0300

    Adição de novos exercícios

--- 2P/POO/Exercicios/Alunos/Professor.java
+++ 2P/POO/Exercicios/Alunos/Professor.java
@@ -1,11 +1,11 @@
 public class Professor {
     private Aluno[] alunos;
-    
+
     // Construtor da classe Professor.
     public Professor(Aluno[] alunos) {
         this.alunos = alunos;
     }
-    
+
     public void exibeSituacaoAluno() {
         for (Aluno aluno : alunos) {
             if (aluno.aprovado()) {
@@ -15,18 +15,17 @@ public class Professor {
             }
         }
     }
-    
-    public Aluno calculaMaiorNota(){
+
+    public Aluno calculaMaiorNota() {
         double maiorNota = 0.0;
         Aluno alunoMaiorNota = new Aluno();
-        
+
         for (int i = 0; i < alunos.length; i++) {
             if (alunos[i].getNota() > maiorNota) {
                 maiorNota = alunos[i].getNota();
                 alunoMaiorNota = alunos[i];
             }
         }
-        
         return alunoMaiorNota;
     }
 }
