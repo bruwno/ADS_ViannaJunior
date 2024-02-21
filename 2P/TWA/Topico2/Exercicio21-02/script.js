b4066bd1c1c54f7ea8477e51748afc8608784287
William Silva <william.silva@viannasempre.com.br>
2024-02-21 12:29:25 -0300

    Adiçãoi de exercícios de TWA

---
+++ 2P/TWA/Topico2/Exercicio21-02/script.js
@@ -0,0 +1,13 @@
+// Verifique se a sua idade é maior do que a de algum parente
+
+var minhaIdade = 26;
+var idadeParente = 18;
+
+console.log("Em relação à idade do meu parente, a minha idade é: ");
+if (minhaIdade > idadeParente) {
+    console.log("É maior");
+} else if (minhaIdade < idadeParente) {
+    console.log("É menor");
+} else {
+    console.log("É igual");
+}
