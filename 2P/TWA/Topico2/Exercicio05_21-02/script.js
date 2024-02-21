0138eba62e1b6374ab9f8ab03a62596018f03eb2
William Silva <william.silva@viannasempre.com.br>
2024-02-21 13:18:20 -0300

    Adição de exercícios de TWA

---
+++ 2P/TWA/Topico2/Exercicio05_21-02/script.js
@@ -0,0 +1,9 @@
+// O que irá aparecer no console?
+if (('Gato' === 'gato') && (5 > 2)) {
+    console.log('Verdadeiro');
+} else {
+    console.log('Falso');
+}
+/* Falso, uma vez que apesar de 5 > 2 ser true, Gato === gato é false, 
+ * já que o operador === exige que os dados comparados sejam exatamente iguais.
+*/
---
+++ 2P/TWA/Topico2/Exercicio06_21-02/script.js
@@ -0,0 +1,10 @@
+// O que irá aparecer no console?
+if (('Gato' === 'gato') || (5 > 2)) {
+    console.log('Gato' && 'gato');
+} else {
+    console.log('Falso');
+}
+
+/* Gato, como estamos utilizando o operador OR, 
+ * a segunda condição avaliada retorna true e entra no bloco de código relativo ao IF.
+ */
