1bae391109fec10b2bf1d901160f84dac6146ad6
William Silva <william.silva@viannasempre.com.br>
2024-02-21 12:57:39 -0300

    Adiçãoi de exercícios de TWA

---
+++ 2P/TWA/Topico2/Exercicio02_21-02/script.js
@@ -0,0 +1,3 @@
+// Qual é o valor retornado da seguinte expressão?
+var expressao = (5 - 2) && (5 - ' ') && (5 - 2); // Operador AND retorna o resultado da última condição avaliada.
+console.log(expressao); // Exibe 3
---
+++ 2P/TWA/Topico2/Exercicio03_21-02/script.js
@@ -0,0 +1,21 @@
+// Verifique se as seguintes variáveis são True ou False
+var nome = 'João';
+var idade = 28;
+var possuiDoutorado = false;
+var empregoFuturo;
+var dinheiroNaConta = 0;
+
+var teste0 = (nome) ? true : false;
+console.log(teste0);
+
+var teste1 = (idade) ? true : false;
+console.log(teste1);
+
+var teste2 = (possuiDoutorado) ? true : false;
+console.log(teste2);
+
+var teste3 = (empregoFuturo) ? true : false;
+console.log(teste3);
+
+var teste4 = (dinheiroNaConta) ? true : false;
+console.log(teste4);
