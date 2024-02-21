4431e15998889fa63f5166cb309c8725ffddbe9c
William Silva <william.silva@viannasempre.com.br>
2024-02-21 13:18:20 -0300

    Adição de exercícios de TWA

---
+++ 2P/TWA/Topico2/Exercicio04_21-02/script.js
@@ -0,0 +1,10 @@
+// Compare o total de habitante do Brasil e da China (valor em mi)
+var brasil = 207;
+var china = 1340;
+
+console.log("Maior população: ");
+if (brasil > china) {
+    console.log("Brasil");
+} else {
+    console.log("China");
+}
