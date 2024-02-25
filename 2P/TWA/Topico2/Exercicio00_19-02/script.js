c6baddbb825bd71313a98a144a128e93e317e011
William Silva <william.silva@viannasempre.com.br>
2024-02-25 10:08:25 -0300

    Adição de novos exercícios de TWA

---
+++ 2P/TWA/Topico2/Exercicio00_19-02/script.js
@@ -0,0 +1,8 @@
+var strNome = "William";
+var strNum = "Nascimento 1997";
+var idade = 26;
+var frase = `Meu nome é ${nome} e minha ${idade} é idade`;
+var sobrenome = "Silva";
+var nomeCompleto = `${nome} ${sobrenome}`;
+var studyTime = "It's study time!";
+console.log(typeof(nome));
