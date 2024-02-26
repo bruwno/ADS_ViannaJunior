265c52c2e268e96771e93d2876c276b3f944ccb3
William Silva <william.silva@viannasempre.com.br>
2024-02-26 13:11:24 -0300

    Adição de novos exercícios de EST

---
+++ 2P/EST/Aulas/Aula02/Fibonacci_Recursivo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Fibonacci_Recursivo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(fib(8));
+        }
+
+        public static int fib(int n)
+        {
+            if (n <= 2)
+                return 1;
+            else
+                return fib(n - 1) + fib(n - 2);
+        }
+    }
+
+}
