475204c2e5c9cf0057eaaa8885c05a8ba036aeb1
William Silva <william.silva@viannasempre.com.br>
2024-02-26 13:11:24 -0300

    Adição de novos exercícios de EST

---
+++ 2P/EST/Aulas/Aula02/Fibonacci_Iterativo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Fibonacci_Iterativo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Fibonacci(8);
+        }
+
+        private static void Fibonacci(int n)
+        {
+            int numAnterior = 1, numAtual = 1;
+
+            for (int i = 3; i <= n; i++)
+            {
+                int aux = numAtual;
+                numAtual = numAtual + numAnterior;
+                numAnterior = aux;
+            }
+            Console.Write(numAtual);
+        }
+    }
+}
