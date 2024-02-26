6e1fcc5e552a5493c12b84ff480a5944910aa5d8
William Silva <william.silva@viannasempre.com.br>
2024-02-26 12:35:35 -0300

    Adição de novos exercícios de EST

--- 2P/EST/Aulas/Aula02/Sort_Recursivo/Program.cs
+++ 2P/EST/Aulas/Aula02/Sort_Recursivo/Program.cs
@@ -6,13 +6,30 @@ namespace Sort_Recursivo
     {
         public static void Main(string[] args)
         {
-            int[] a = new int[] { 1002, 1, 59, 10100, 45, 72, 2, 5 };
+            int[] a = new int[] { 59, 10100, 72, 2 };
             Sort(a);
+
+            foreach (int num in a)
+            {
+                Console.Write(num + " ");
+            }
         }
 
         public static void Sort(int[] a)
         {
             Sort1(0, a);
+        }
+
+        private static void Min(int k, int[] a, ref int j)
+        {
+            if (k < a.Length)
+            {
+                if (a[k] < a[j])
+                {
+                    j = k;
+                }
+                Min(k + 1, a, ref j);
+            }
         }
 
         private static void Sort1(int i, int[] a)
@@ -28,20 +45,6 @@ namespace Sort_Recursivo
                 a[j] = t;
                 Sort1(i + 1, a);
             }
-
-            // Console.WriteLine(a);
-        }
-
-        private static void Min(int k, int[] a, ref int j)
-        {
-            if (k < a.Length)
-            {
-            }
-            if (a[k] < a[j])
-            {
-                j = k;
-            }
-            Min(k + 1, a, ref j);
         }
     }
 }
