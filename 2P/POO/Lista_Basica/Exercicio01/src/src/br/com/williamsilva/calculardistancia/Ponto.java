8cc51883c6aae200fabbf3af730c2ce0f3f8d624
WILLIAM Silva <william.silva@viannasempre.com.br>
2024-02-27 18:34:00 -0300

    Update Ponto.java

--- 2P/POO/Lista_Basica/Exercicio01/src/src/br/com/williamsilva/calculardistancia/Ponto.java
+++ 2P/POO/Lista_Basica/Exercicio01/src/src/br/com/williamsilva/calculardistancia/Ponto.java
@@ -3,6 +3,8 @@ package br.com.williamsilva.calculardistancia;
 public class Ponto {
     private double x;
     private double y;
+
+    public Ponto() {}
     
     public Ponto(double x, double y) {
         this.x = x;
