917131fcae4dceb3d1b199f9039a3564514348c5
William Silva <william.silva@viannasempre.com.br>
2024-02-29 08:15:07 -0300

    Adição de exercícios de EST e TWA

---
+++ 2P/EST/Aulas/Aula02/Exercicio05/Program.cs
@@ -0,0 +1,32 @@
+﻿/* Faça uma função recursiva que receba um vetor de interios por parâmetro
+ * e retorna a soma dos elementos pertencentes a este vetor.
+ */
+
+using System;
+
+namespace Exercicio05
+{
+    public class Program
+    {
+        public static void Main(string[] args) 
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine(RetornaSoma(array, 0, 0));
+        }
+
+        public static int RetornaSoma(int[] array, int i, int soma = 0) 
+        {
+            if (i >= array.Length) 
+            {
+                return soma;
+                // return 0;
+            }
+            else 
+            {
+                soma += array[i];
+                return RetornaSoma(array, i + 1, soma);
+                // return array[i] + RetornaSoma(array, i + 1);
+            }
+        }
+    }
+}
---
+++ 2P/TWA/Topico3/Exercicio04-28_02/script.js
@@ -0,0 +1,9 @@
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+var j = 1;
+while (i < 10) {
+    console.log(j);
+    j++;
+}
