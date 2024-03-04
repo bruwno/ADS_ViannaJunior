384ecfa8f11c382e122117bf79610962b2973b29
William Silva <william.silva@viannasempre.com.br>
2024-03-04 14:34:41 -0300

    Adição de novos exercícios de EST

---
+++ 2P/EST/Ordenacao/BubbleSort/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double[] vet = new double[] { 1, 2, 5, 4, 10, 9, 8 };
+            BubbleSort(vet);
+
+            foreach (var num in vet)
+            {
+                Console.Write($"{num} ");
+            }
+        }
+
+        public static void BubbleSort(double[] vetor)
+        {
+            int fim = vetor.Length - 1, pos = 0;
+            bool troca = true;
+
+            while (troca)
+            {
+                troca = false;
+                for (int i = 0; i < fim; i++)
+                {
+                    if (vetor[i] > vetor[i + 1])
+                    {
+                        Troca(vetor, i, i + 1);
+                        pos = i;
+                        troca = true;
+                    }
+                }
+                fim = pos - 1;
+            }
+        }
+
+        private static void Troca(double[] vetor, int i, int j)
+        {
+            double aux = vetor[i];
+            vetor[i] = vetor[j];
+            vetor[j] = aux;
+        }
+    }
+}
