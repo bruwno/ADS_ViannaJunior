6bc0dac13bfb3b296425db10a99941ce3c0dce8f
William Silva <william.silva@viannasempre.com.br>
2024-03-03 21:06:21 -0300

    Alteração no exercício 5 de POO

--- 2P/POO/Solucoes_Lista_Basica/Exercicio05/src/Main.java
+++ 2P/POO/Solucoes_Lista_Basica/Exercicio05/src/Main.java
@@ -11,11 +11,14 @@ import br.com.williamsilva.calctriangulo.Triangulo;
 public class Main {
     public static void main(String[] args) {
         double[] lados = LerLados.leRetornaLados();
-        Triangulo triangulo = new Triangulo(lados[0], lados[1], lados[2]);
+        Triangulo triangulo = new Triangulo();
+        triangulo.setA(lados[0]);
+        triangulo.setB(lados[1]);
+        triangulo.setC(lados[2]);
 
         System.out.println("Características do triângulo: ");
-        System.out.println("Área = " + triangulo.calculaAreaTriangulo());
+        System.out.println("Área      = " + triangulo.calculaAreaTriangulo());
         System.out.println("Perímetro = " + triangulo.calculaPerimetroTriangulo());
-        System.out.println("Tipo = " + triangulo.defineTipoTriangulo());
+        System.out.println("Tipo      = " + triangulo.defineTipoTriangulo());
     }
 }
