6386cd0dddc3aca2af5cc761e7fc8d202eba3344
William Silva <william.silva@viannasempre.com.br>
2024-03-06 07:47:03 -0300

    Adição de exercícios de DID a BDA

--- 2P/BDA/Exercicios/Exercicio_Semana5/hospital_Exercicio.sql
+++ 2P/BDA/Exercicios/Exercicio_Semana5/hospital_Exercicio.sql
@@ -15,7 +15,7 @@ SELECT * FROM doenca WHERE descricao IS NULL;
 
 -- Tabela hospital
 -- 3) Recupere as informações dos hospitais situados no bairro São Mateus da cidade de Juiz de Fora.
-SELECT * FROM hospital WHERE bairro = 'São Mateus';
+SELECT * FROM hospital WHERE bairro = 'São Mateus' AND cidade = 'Juiz de Fora';
 
 -- Tabela medico
 -- 4) Recupere as informações dos médicos que moram na Av. Barão do Rio Branco.
@@ -24,3 +24,15 @@ SELECT * FROM medico WHERE rua = 'Av. Barão do Rio Branco';
 -- Tabela paciente
 -- 5) Recupere as informações dos pacientes que nasceram após 01/01/1990.
 SELECT * FROM paciente WHERE datanasc > '1990-01-01';
+
+-- Tabela especialidade
+-- 6) Recupere as informações da especialidade de nome Dermatologia
+SELECT * FROM especialidade WHERE especialidade = 'Dermatologia';
+
+-- Tabela diagnostico
+-- 7) Recupere as informações do diagnóstico do paciente 12345678901.
+SELECT * FROM diagnostico WHERE cpf = 12345678901;
+
+-- Tabela medico_hospital
+-- 8) Recupere as informações da tabela medico_hospital, cuja data de início do médico está entre 01/01/2000 até 08/02/2003.
+SELECT * FROM medico_hospital WHERE dataInicio >= '2000-01-01' AND dataInicio <= '2003-02-08';
