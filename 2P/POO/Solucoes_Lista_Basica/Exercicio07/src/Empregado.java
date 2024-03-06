761025fdbacf83a6a42c515f109207993fc13b78
William Silva <william.silva@viannasempre.com.br>
2024-03-06 12:48:03 -0300

    Alteração no exercício 7 da lista de POO

--- 2P/POO/Solucoes_Lista_Basica/Exercicio07/src/Empregado.java
+++ 2P/POO/Solucoes_Lista_Basica/Exercicio07/src/Empregado.java
@@ -46,21 +46,15 @@ public class Empregado {
         return String.format("Nome: %s %nIdade: %d anos de idade %nTempo de Serviço: %d anos", nome, idade, tempoDeServico);
     }
 
-    public String verificaSituacaoAposentadoria() {
-        String situacaoAposentadoria;
-        if (podeAposentarPorIdade() || podeAposentarPorTempoDeServico()) {
-            situacaoAposentadoria = "Está qualificado para aposentadoria";
-        } else {
-            situacaoAposentadoria = "Ainda não pode se aposentar";
-        }
-        return situacaoAposentadoria;
+    public boolean verificaSituacaoAposentadoria() {
+        return podeAposentarPorIdade() || podeAposentarPorTempoDeServico();
     }
 
-    protected boolean podeAposentarPorIdade() {
+    public boolean podeAposentarPorIdade() {
         return idade >= 65 && tempoDeServico >= 30;
     }
 
-    protected boolean podeAposentarPorTempoDeServico() {
+    public boolean podeAposentarPorTempoDeServico() {
         return idade >= 60 && tempoDeServico >= 25;
     }
 }
