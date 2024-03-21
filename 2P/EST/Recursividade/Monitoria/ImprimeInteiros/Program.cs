bf0ee68e2119f3a9362c80e5c4fb6f2dde8eb391
William Silva <william.silva@viannasempre.com.br>
2024-03-21 12:50:42 -0300

    Adição de novos exercícios de EST

---
+++ 2P/EST/Recursividade/Monitoria/ImprimeInteiros/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Exercício 02
+ * Faça um procedimento recursivo que imprima todos os elementos de um vetor de inteiros
+ * separados por '-' (traço). Observação: traços só podem ser impressos se estiverem separando elementos
+ * do vetor, ou seja, não pode haver traço no início ou no final da impressão.
+ */
+
+using System;
+
+namespace ImprimeInteiros 
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] vet = new int[] { 1, 2, 3, 4, 5 };
+            SeparaPorTraco(vet);
+        }
+
+        public static void SeparaPorTraco(int[] vet, int i = 0) {
+            if (i == vet.Length - 1) 
+            {
+                Console.Write(vet[i]);
+            }
+            else if (i >= 0 && i < vet.Length - 1)
+            {
+                Console.Write($"{vet[i]}-");
+                SeparaPorTraco(vet, i + 1);
+            }
+        }
+
+    }
+    
+}
---
+++ 2P/EST/Recursividade/Monitoria/SomaPares/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Exercício: Faça uma função recursiva que retorne a soma dos elementos pares, existentes em um vetor de inteiros.
+ * Data: 18/06/2024
+ */
+
+using System;
+
+namespace SomaPares
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] vet = new int[] { 0, 1, 1, 1, 11, 9, 2, 0, 100 };
+            Console.Write($"Resultado= {SomaPares(0, vet, 0)}");
+        }
+
+        public static int SomaPares(int i, int[] vet, int soma)
+        {
+          if (i >= vet.Length)
+            {
+                return soma;
+            }
+            else
+            {
+                if (vet[i] % 2 == 0) 
+                {
+                    soma += vet[i];
+                }
+                return SomaPares(i+1, vet, soma);
+            }
+        }
+    }
+}
