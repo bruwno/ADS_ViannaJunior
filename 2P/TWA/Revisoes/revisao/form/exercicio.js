aa2fe66fd6541548a67814fd377e7789e2332bc6
William Silva <william.silva@viannasempre.com.br>
2024-03-30 17:24:56 -0300

    Adição de rescolução de exercícios de TWA

--- 2P/TWA/Revisoes/revisao/form/exercicio.js
+++ 2P/TWA/Revisoes/revisao/form/exercicio.js
@@ -1,2 +1,18 @@
 // Ao clicar no botão + deve ser adicionado um novo grupo para adicionar um telefone
+let btnAdicionarTelefone = document.querySelector(".btn-add");
+
+btnAdicionarTelefone.addEventListener("click", event => {
+    let phoneGroup = document.querySelector(".phoneGroup");
+    let novoCampoTelefone = phoneGroup.cloneNode(true);
+    phoneGroup.parentNode.insertBefore(novoCampoTelefone, phoneGroup.nextSibling);
+
+});
+
+
 // Ao clicar no botão Novo contato a classe btn-primary deve ser substituida por btn-success
+let botaoNovoContato = document.getElementById("newContact");;
+
+botaoNovoContato.addEventListener("click", event => {
+    event.preventDefault();
+    botaoNovoContato.classList.replace("btn-primary", "btn-success");
+});
