fef2012ca844bdc1ba2ee3b770a846be1a56eaaa
William Silva <william.silva@viannasempre.com.br>
2024-03-31 16:45:54 -0300

    Adição de aulas do curso de JS da B7Web. Revisão para TWA.

---
+++ 2P/TWA/_RecursosExternos/B7Web_JavaScript/Modulo1/Aula18/assets/js/script.js
@@ -0,0 +1,24 @@
+// Substitua o @ pela condicional correta para que o resultado seja TRUE
+let x = 10;
+let y = 5;
+console.log(x > y);
+
+// Substitua o @ pela condicional correta para que o resultado seja FALSE
+let w = "10";
+let z = 10;
+console.log(w === z);
+
+// Substitua o @ pela condicional correta para que o resultado seja TRUE
+let a = 10;
+let b = 5;
+console.log(a > b);
+
+// Crie uma condicional para verificar se o preço da carne está barato ou caro.
+// Até 45 está barato.
+let preco = 40.99;
+
+if (preco <= 40.0) {
+    console.log("O preço da carne está barato");
+} else {
+    console.log("O preço da carne está caro.");
+}
