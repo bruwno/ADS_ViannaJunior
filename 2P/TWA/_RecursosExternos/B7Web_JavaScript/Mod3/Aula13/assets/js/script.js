95c9660bc302ecb47f6290bdde25c29ec7f55edd
William Silva <william.silva@viannasempre.com.br>
2024-04-03 16:03:49 -0300

    Adição de novos exercícios de TWA

--- 2P/TWA/_RecursosExternos/B7Web_JavaScript/Mod3/Aula13/assets/js/script.js
+++ 2P/TWA/_RecursosExternos/B7Web_JavaScript/Mod3/Aula13/assets/js/script.js
@@ -12,9 +12,22 @@ function handleKeyUp(e) {
 input.addEventListener("keyup", handleKeyUp);
 
 function addItem() {
-    let newLi = document.createElement("li");
-    newLi.innerHTML = input.value;
-    ul.appendChild(newLi);
+    let newLi = document.createElement("li"); // Criando o elemento li na ul.
+    newLi.innerHTML = input.value; // Fazendo a nova li receber o valor do input.
+    ul.appendChild(newLi); // Adicionando o elemento no documento.
 
     input.value = ""; // Limpando o input.
 }
+
+let btnDelete = document.createElement("button");
+document.body.appendChild(btnDelete);
+btnDelete.textContent = "Remover item";
+
+
+function removeItem() {
+    ul.removeChild(ul.lastElementChild);
+}
+
+btnDelete.addEventListener("click", () => {
+    removeItem();
+});
