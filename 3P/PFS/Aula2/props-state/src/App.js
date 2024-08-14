8cca3e36ca8309278af336b7c6d3163c1ca55096
William Silva <william.silva@viannasempre.com.br>
2024-08-14 12:43:47 -0300

    Adição do app feito em React.

--- 3P/PFS/Aula2/props-state/src/App.js
+++ 3P/PFS/Aula2/props-state/src/App.js
@@ -1,23 +1,49 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Evento from "./components/Evento";
+import './App.css';
+import { faker } from '@faker-js/faker';
+
+import Evento from './components/Evento';
 
 function App() {
   return (
-    <div className="ui feed">
-      {objetos.map((x) => {
-        return <Evento nome={x.nome} likes={x.likes} />;
-      })}
+    <div className='ui feed'>
+      {
+        objetos.map(x => {
+          return <Evento nome={x.nome} likes={x.likes} mensagem={x.mensagem} fotoPerfil={x.fotoPerfil} tempo={x.tempo} />
+        })
+      }
     </div>
   );
 }
 
 const objetos = [
-  { nome: "William", likes: 10 },
-  { nome: "Bruno", likes: 15 },
-  { nome: "Samuelzim", likes: 70 },
-  { nome: "Otaviozim", likes: 50 },
-  { nome: "Renanzim", likes: 45 },
+  {
+    nome: faker.person.firstName(),
+    likes: faker.number.int(),
+    mensagem: faker.lorem.sentence(),
+    fotoPerfil: faker.image.avatar(),
+    tempo: faker.number.int()
+  },
+  {
+    nome: faker.person.firstName(),
+    likes: faker.number.int(),
+    mensagem: faker.lorem.sentence(),
+    fotoPerfil: faker.image.avatar(),
+    tempo: faker.number.int()
+  },
+  {
+    nome: faker.person.firstName(),
+    likes: faker.number.int(),
+    mensagem: faker.lorem.sentence(),
+    fotoPerfil: faker.image.avatar(),
+    tempo: faker.number.int()
+  },
+  {
+    nome: faker.person.firstName(),
+    likes: faker.number.int(),
+    mensagem: faker.lorem.sentence(),
+    fotoPerfil: faker.image.avatar(),
+    tempo: faker.number.int()
+  },
 ];
 
 export default App;
--- 3P/PFS/Aula2/props-state/src/components/Evento.js
+++ 3P/PFS/Aula2/props-state/src/components/Evento.js
@@ -1,26 +1,31 @@
 import React from "react";
-import { faker } from "@faker-js/faker";
+import { faker } from '@faker-js/faker';
 
 const Evento = (props) => {
-  return (
-    <div className="event">
-      <div className="label">
-        <img src={faker.image.avatar()} />
-      </div>
-      <div className="content">
-        <div className="summary">
-          <a className="user">{props.nome}</a> added you as a
-          friend
-          <div className="date">1 Hour Ago</div>
+    return(
+        <div className="ui feed">
+      <div className="event">
+        <div className="label">
+          <img src={props.fotoPerfil}/>
         </div>
-        <div className="meta">
-          <a className="like">
-            <i className="like icon"></i> {props.likes}
-          </a>
+        <div className="content">
+          <div className="summary">
+            <a className="user">
+              {props.nome}
+            </a> {props.mensagem}
+            <div className="date">
+              {props.tempo}
+            </div>
+          </div>
+          <div className="meta">
+            <a className="like">
+              <i className="like icon"></i> {props.likes} Likes
+            </a>
+          </div>
         </div>
       </div>
     </div>
-  );
+    )
 };
 
 export default Evento;
