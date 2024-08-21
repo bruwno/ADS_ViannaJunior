1bc28f9e56c81dc3a9f12e396e265c31d9db687e
William Silva <william.silva@viannasempre.com.br>
2024-08-21 16:42:40 -0300

    Alterações no código da aplicação Calculadora

--- 3P/DAM/FirstApp/firstApp/App.js
+++ 3P/DAM/FirstApp/firstApp/App.js
@@ -1,145 +1,166 @@
-// import { StatusBar } from 'expo-status-bar';
-// import { useState } from 'react';
-// import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-
-// export default function App() {
-//   // Declaração de variável useState.
-//   // Manipula a variável e o valor que será exibido na tela.
-//   const [v1, setV1] = useState(0) //  é o valor de inicialização da variável.
-//   const [v2, setV2] = useState(5)
-  
-//   const calcular = () =>{
-//     setV1(v1 + 1); // Executa de forma assíncrona.
-//     setV2(v2 + 1);
-//     console.log(val1, val2);
-//   }
-
-//   return (
-//     <View style={styles.container}>
-//       <Text style={styles.titulo}>Calculadora</Text>
-
-//       <View style={styles.body}>
-//         <Text style ={styles.texto}>Valor 1</Text>
-//         <TextInput style ={styles.campo} value={val1}/>
-//         <Text style ={styles.texto}>Valor 2</Text>
-//         <TextInput style ={styles.campo} value={val2}/>
-//       </View>
-
-//       <TouchableOpacity onPress={calcular} style ={styles.btn}>
-//         Calcular
-//       </TouchableOpacity>
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   btn: {
-//     padding: 25,
-//     fontSize: 32,
-//     backgroundColor: '#2B9EB3',
-//     borderRadius: 15,
-//     marginBottom: 10,
-//     width: '98%',
-//     textAlign: 'center'
-//   },
-//   body: {
-//     flexGrow: 1,
-//     //backgroundColor: '#ff0',
-//   },
-//   container: {                                         
-//     flex: 1,
-//     backgroundColor: '#AA9',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   texto: {
-//     fontSize: 20,
-//     paddingBottom: 10,
-//     paddingTop: 10,
-//   },
-//   campo: {
-//     backgroundColor: '#fff',
-//     fontSize: 22,
-//     padding: 5,
-//   },
-//   titulo: {
-//     fontSize: 48,
-//     fontWeight: 'bold',
-//     padding: 25,
-//   }
-// });
-
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [pValor, setPValor] = useState(0);
+  const [sValor2, setSValor] = useState(0);
+  const [resultado, setResultado] = useState(0);
 
-  //let v1 = 0;
-  //let v2 = 5;
-  const [v1, setV1] = useState(0);
-  const [v2, setV2] = useState(5);
+  const calcular = () => {
+    const val1 = parseFloat(pValor);
+    const val2 = parseFloat(sValor2);
 
-  const calcular = () =>{
-    setV1 (v1 + 1);
-    setV2 (v2 + 1);
-    //v2 = v2 + 1;
-    console.log("OK ", v1, v2);
-  }
+    if (!isNaN(val1) && !isNaN(val2)) {
+      setResultado(val1 + val2);
+    } else {
+      setResultado("Entrada inválida");
+    }
+    console.log("OK ", pValor, sValor2);
+  };
+
+  const limparCampos = () => {
+    if (pValor !== "" || sValor2 !== "") {
+      setPValor(0);
+      setSValor(0);
+      setResultado(0);
+    }
+  };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.titulo}>Calculadora</Text>
-
-      <View style={styles.body}>
-        <Text style ={styles.texto}>Valor 1</Text>
-        <TextInput style ={styles.campo} value={v1}/>
-        <Text style ={styles.texto}>Valor 2</Text>
-        <TextInput style ={styles.campo} value={v2}/>
+      <View style={styles.titulo}>
+        <Text style={styles.titulo}>Calculadora</Text>
       </View>
 
-      <TouchableOpacity onPress={calcular} style ={styles.btn}>
-        Calcular
-      </TouchableOpacity>
+      <View style={styles.body}>
+        <Text style={styles.texto}>Primeiro valor</Text>
+        <TextInput
+          style={styles.campo}
+          value={String(pValor)}
+          onChangeText={(text) => setPValor(text)}
+        />
+
+        <Text style={styles.texto}>Segundo valor</Text>
+        <TextInput
+          style={styles.campo}
+          value={String(sValor2)}
+          onChangeText={(text) => setSValor(text)}
+        />
+      </View>
+
+      <View style={styles.resultadoContainer}>
+        <Text style={styles.resultadoTitulo}>Resultado</Text>
+        <TextInput
+          style={styles.resultadoCampo}
+          value={String(resultado)}
+          editable={false}
+        />
+      </View>
+
+      <View style={styles.btnContainer}>
+        <TouchableOpacity onPress={calcular} style={styles.btnCalcular}>
+          <Text style={styles.btnCalcular}>Calcular</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={limparCampos} style={styles.btnLimpar}>
+          <Text style={styles.btnLimpar}>Limpar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  btn: {
-    padding: 25,
-    fontSize: 32,
-    backgroundColor: '#0d6007',
-    borderRadius: 15,
-    marginBottom: 10,
-    width: '98%',
-    textAlign: 'center'
-  },
   body: {
     flexGrow: 1,
-    //backgroundColor: '#ff0',
   },
-  container: {                                         
+  container: {
     flex: 1,
-    backgroundColor: '#AA9',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F5F5F5",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  titulo: {
+    color: "#212121",
+    fontSize: 40,
+    fontWeight: "bold",
+    padding: 25,
+    backgroundColor: "#E0E0E0",
+    width: "100%",
+    textAlign: "center",
+    borderRadius: 15,
   },
   texto: {
     fontSize: 20,
+    color: "#212121",
     paddingBottom: 10,
     paddingTop: 10,
+    textAlign: "center",
   },
   campo: {
-    backgroundColor: '#fff',
+    backgroundColor: "#E0E0E0",
+    borderColor: "#424242",
+    borderRadius: 5,
     fontSize: 22,
     padding: 5,
+    marginBottom: 10,
+    textAlign: "center",
   },
-  titulo: {
-    fontSize: 48,
-    fontWeight: 'bold',
-    padding: 25,
-
-  }
+  btnContainer: {
+    backgroundColor: "#E0E0E0",
+    padding: 10,
+    width: "100%",
+    borderWidth: 2,
+    borderColor: "#E0E0E0",
+    borderRadius: 15,
+    alignContent: "center",
+    alignItems: "center",
+  },
+  btnCalcular: {
+    color: "#F5F5F5",
+    padding: 5,
+    fontSize: 32,
+    backgroundColor: "#424242",
+    borderRadius: 15,
+    marginBottom: 10,
+    width: "95%",
+    textAlign: "center",
+  },
+  btnLimpar: {
+    color: "#212121",
+    padding: 2,
+    fontSize: 20,
+    backgroundColor: "#8B7D7B",
+    borderRadius: 15,
+    marginBottom: 5,
+    width: "95%",
+    textAlign: "center",
+  },
+  resultadoContainer: {
+    alignItems: "center",
+    marginVertical: 20,
+  },
+  resultadoTitulo: {
+    fontSize: 20,
+    marginBottom: 10,
+  },
+  resultadoCampo: {
+    backgroundColor: "#E0E0E0",
+    fontSize: 40,
+    fontWeight: "bold",
+    width: "90%",
+    height: 70,
+    textAlign: "center",
+    padding: 10,
+    borderRadius: 5,
+    borderWidth: 1.5,
+    borderColor: "#424242",
+    margin: 10,
+  },
 });
