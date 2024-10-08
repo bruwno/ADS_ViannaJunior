5216091c0ef5c468d27cf4113c82f3293f985799
William Silva <william.silva@viannasempre.com.br>
2024-10-08 13:35:32 -0300

    Remoção do projeto WebTask

---
+++ 3P/TMS/Etapa03_WabTask/aula-main-webTaskClean/webTaskClean/src/test/java/br/com/webtask/aula/domain/model/EStatusTest.java
@@ -0,0 +1,8 @@
+package br.com.webtask.aula.domain.model;
+/**
+ * @author William
+ * @date 05/10/2024
+ */
+
+public class EStatusTest {
+}
---
+++ 3P/TMS/Etapa03_WabTask/aula-main-webTaskClean/webTaskClean/src/test/java/br/com/webtask/aula/domain/repo/TaskRepoTest.java
@@ -0,0 +1,8 @@
+package br.com.webtask.aula.domain.repo;
+/**
+ * @author William
+ * @date 06/10/2024
+ */
+
+public class TaskRepoTest {
+}
