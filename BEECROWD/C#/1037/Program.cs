1f01b09d71973bc20ff5015355e22025f584957f
William Silva <william.silva@viannasempre.com.br>
2025-03-15 10:30:08 -0300

    Adicionando as soluções para dos desafios 1037 e 1096 do Beecrowd

---
+++ BEECROWD/C#/1037/Program.cs
@@ -0,0 +1,43 @@
+﻿/* Objetivo: Ler um valor de ponto flutuante e retornar o intervalo ao qual ele pertence.
+ * Autor   : William Silva (https://github.com/bruwno)
+ * Data    : 14/03/2025
+ * Tipo    : 1 (INICIANTE)
+ * ID      : 1037
+ * Nome    : Intervalo
+ * Link    : https://www.beecrowd.com.br/judge/pt/problems/view/1037
+ */
+
+using System;
+
+class URI
+{
+    public static void Main(string[] args)
+    {
+        double entrada = double.Parse(Console.ReadLine());
+        Console.WriteLine(VerificarIntervalo(entrada));
+    }
+
+    public static string VerificarIntervalo(double valor)
+    {
+        if (valor >= 0.00 && valor <= 25.00)
+        {
+            return "Intervalo [0,25]";
+        }
+        else if (valor >= 25.01 && valor <= 50.00)
+        {
+            return "Intervalo (25,50]";
+        }
+        else if (valor >= 50.01 && valor <= 75.00)
+        {
+            return "Intervalo (50,75]";
+        }
+        else if (valor >= 75.01 && valor <= 100.00)
+        {
+            return "Intervalo (75,100]";
+        }
+        else
+        {
+            return "Fora de intervalo";
+        }
+    }
+}
---
+++ BEECROWD/C#/1096/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Objetivo: Apresentar uma sequência numérica.
+ * Autor   : William Silva (https://github.com/bruwno)
+ * Data    : 14/03/2025
+ * Tipo    : 1 (INICIANTE)
+ * ID      : 1096
+ * Nome    : Sequência IJ 2
+ * Link    : https://www.beecrowd.com.br/judge/pt/problems/view/1096
+ */
+
+using System;
+
+class URI
+{
+    public static void Main(string[] args)
+    {
+        for (int cont = 0, i = 1, j = 7; cont < 11; cont++)
+        {
+            i = cont;
+            j = 7;
+            int contRepeticoes = 3;
+
+            if (cont < 15 && ehImpar(i))
+            {
+                while (contRepeticoes > 0)
+                {
+                    Console.WriteLine($"I={i} J={j}");
+                    j--;
+                    contRepeticoes--;
+                }
+            }
+        }
+    }
+
+    public static bool ehImpar(int n)
+    {
+        return n % 2 == 1;
+    }
+}
