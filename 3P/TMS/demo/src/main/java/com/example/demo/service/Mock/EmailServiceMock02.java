dc257ccd3e4c243c0549189775187846005cc8fe
William Bruno <william.silva@viannasempre.com.br>
2025-07-14 17:19:36 -0300

    Remove deprecated files and add mock email services for testing

---
+++ 3P/TMS/demo/src/main/java/com/example/demo/service/Mock/EmailServiceMock02.java
@@ -0,0 +1,11 @@
+package com.example.demo.service.Mock;
+
+import com.example.demo.service.EmailService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceMock02 extends EmailService {
+    public void sendEmail(String email, String titulo, String corpo) throws Exception {
+        throw new Exception("538 - Destinário Inválido");
+    }
+}
